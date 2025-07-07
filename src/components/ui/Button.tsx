395656e5db5aecd4fcc2e871bc439768b484b27e
Jungyu Park <parkjungyu09@gmail.com>
2025-07-07 22:34:43 +0900

    Style: 공통 버튼 컴포넌트 디자인 구현

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -1,3 +1,29 @@
-export const Button = () => {
-    return <button>버튼</button>
+import { ButtonHTMLAttributes } from "react";
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  children: React.ReactNode;
+  state?: "default" | "disabled";
 }
+
+export const Button = ({
+  children,
+  state = "default",
+  className = "",
+  ...props
+}: ButtonProps) => {
+  const baseStyle = "w-full h-[50px] rounded-lg text-sm";
+
+  const stateStyles = {
+    default: "bg-black text-white",
+    disabled: "bg-white text-black",
+  };
+  return (
+    <button
+      className={`${baseStyle} ${stateStyles[state]} ${className}`}
+      disabled={state === "disabled"}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
