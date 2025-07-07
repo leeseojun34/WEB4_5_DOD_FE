7243ab891e520cc42bac9294e6f46474f9e59f00
Suji <soojiya2003@gmail.com>
2025-07-08 01:23:28 +0900

    Design : 색상값 변수명으로 변경

--- src/components/ui/Tip.tsx
+++ src/components/ui/Tip.tsx
@@ -1,8 +1,8 @@
 const Tip = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="w-full flex flex-col px-5 py-4 gap-2 text-[#338aff] bg-[#f2f6fe] rounded-lg text-xs leading-[21px]">
-      <p>이때, Tip</p>
-      <p>{children}</p>
+    <div className="w-full flex flex-col px-5 py-4 gap-2 text-[var(--color-primary-400)] bg-[var(--color-primary-100)] rounded-lg text-xs leading-[21px]">
+      <p className="font-medium">이때, Tip</p>
+      <p className="font-normal">{children}</p>
     </div>
   );
 };
