7b86f46e68bca04c5046b086b12ac271559ab165
snowari <70631889+snowari@users.noreply.github.com>
2025-07-08 02:46:32 +0900

    Style : add input component

--- src/components/landing/Landing.tsx
+++ src/components/landing/Landing.tsx
@@ -2,8 +2,12 @@
 
 import { useState } from "react";
 import BottomSheet from "../ui/BottomSheet";
+import { Search } from "lucide-react";
+import Input from "@/components/ui/Input";
 const Landing = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [text, setText] = useState("");
+  const [errorText, setErrorText] = useState("");
 
   return (
     <>
@@ -13,6 +17,45 @@ const Landing = () => {
       >
         Open sheet
       </button>
+
+      <div className="p-4 w-[335px] flex flex-col gap-6">
+        <Input
+          label="기본 Input"
+          placeholder="placeholder"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+
+        <Input
+          placeholder="placeholder"
+          icon={<Search className="w-4 h-4 " />}
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+
+        <Input
+          label="멀티라인"
+          isTextarea
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          placeholder="placeholder"
+          maxLength={50}
+        />
+
+        <Input
+          label="에러 Input"
+          placeholder="입력하세요"
+          value={errorText}
+          onChange={(e) => {
+            if (e.target.value.length <= 10) {
+              setErrorText(e.target.value);
+            }
+          }}
+          maxLength={10}
+          error="최대 글자 수 초과"
+        />
+      </div>
+
       <BottomSheet isOpen={isOpen} setIsOpen={setIsOpen}>
         <div className="p-4">Hello from the bottom sheet</div>
       </BottomSheet>
---
+++ src/components/ui/Input.tsx
@@ -0,0 +1,161 @@
+import classNames from "classnames";
+import {
+  InputHTMLAttributes,
+  ReactNode,
+  TextareaHTMLAttributes,
+  useEffect,
+  useState,
+} from "react";
+
+type BaseProps = {
+  label?: string;
+  error?: string;
+  icon?: ReactNode;
+  maxLength?: number;
+  isTextarea?: boolean;
+  fullWidth?: boolean;
+};
+
+type InputProps = BaseProps &
+  (
+    | ({ isTextarea?: false } & InputHTMLAttributes<HTMLInputElement>)
+    | ({ isTextarea?: true } & TextareaHTMLAttributes<HTMLTextAreaElement>)
+  );
+
+const Input = ({
+  label,
+  error,
+  icon,
+  maxLength,
+  isTextarea = false,
+  fullWidth = false,
+  className,
+  value = "",
+  ...props
+}: InputProps) => {
+  const valueLength = typeof value === "string" ? value.length : 0;
+  const [isFocused, setIsFocused] = useState(false);
+  const [showMaxLengthError, setShowMaxLengthError] = useState(false);
+
+  useEffect(() => {
+    if (showMaxLengthError) {
+      const timer = setTimeout(() => {
+        setShowMaxLengthError(false);
+      }, 1000); //에러메세지 1초 동안
+      return () => clearTimeout(timer);
+    }
+  }, [showMaxLengthError]);
+
+  const keyDownHandler = (e: React.KeyboardEvent) => {
+    if (
+      maxLength !== undefined &&
+      valueLength >= maxLength &&
+      e.key !== "Backspace" &&
+      e.key !== "Delete"
+    ) {
+      setShowMaxLengthError(true);
+    }
+    if (props.onKeyDown) {
+      props.onKeyDown(e);
+    }
+  };
+
+  //label, input, error 랩
+  const containerClass = classNames(
+    "flex flex-col gap-2",
+    fullWidth && "w-full"
+  );
+
+  //input 랩
+  const inputWrapperClass = classNames(
+    "w-full flex items-center gap-2.5 px-4 py-4 rounded-[8px]",
+    "border border-[var(--color-gray-placeholder)]",
+    "focus-within:border-[var(--color-primary-400)]",
+    className,
+    isTextarea && "relative"
+  );
+
+  //기본 스타일
+  const inputBaseClass = classNames(
+    "flex-1 outline-none text-sm placeholder:text-[var(--color-gray-placeholder)]",
+    isTextarea ? "min-h-[100px] pr-12 " : "h-4"
+  );
+
+  return (
+    <div className={containerClass}>
+      {label && <label className="text-xs font-medium">{label}</label>}
+      <div className={inputWrapperClass}>
+        {isTextarea ? (
+          <>
+            <textarea
+              className={inputBaseClass}
+              maxLength={maxLength}
+              value={value}
+              onKeyDown={keyDownHandler}
+              {...(props as TextareaHTMLAttributes<HTMLTextAreaElement>)}
+            />
+            {maxLength !== undefined && (
+              <span
+                className={classNames(
+                  "absolute right-4 bottom-4 text-xs",
+                  valueLength === 0
+                    ? "text-[var(--color-gray-placeholder)]"
+                    : "text-[var(--color-primary-400)]"
+                )}
+              >
+                {valueLength}/{maxLength}
+              </span>
+            )}
+          </>
+        ) : (
+          <>
+            <input
+              className={inputBaseClass}
+              maxLength={maxLength}
+              value={value}
+              {...(props as InputHTMLAttributes<HTMLInputElement>)}
+              onFocus={(e) => {
+                setIsFocused(true);
+                if (props.onFocus) props.onFocus(e);
+              }}
+              onBlur={(e) => {
+                setIsFocused(false);
+                if (props.onBlur) props.onBlur(e);
+              }}
+              onKeyDown={keyDownHandler}
+            />
+            {icon && (
+              <span
+                className={
+                  isFocused
+                    ? "text-[var(--color-primary-400)]"
+                    : "text-[var(--color-gray-placeholder)]"
+                }
+              >
+                {icon}
+              </span>
+            )}
+            {maxLength !== undefined && (
+              <span
+                className={classNames(
+                  "text-xs ml-auto",
+                  valueLength === 0
+                    ? "text-[var(--color-gray-placeholder)]"
+                    : "text-[var(--color-primary-400)]"
+                )}
+              >
+                {valueLength}/{maxLength}
+              </span>
+            )}
+          </>
+        )}
+      </div>
+      {showMaxLengthError && (
+        <p className="text-xs text-[var(--color-red)] mt-1">
+          *{error || "최대 글자 수 초과"}
+        </p>
+      )}
+    </div>
+  );
+};
+export default Input;
