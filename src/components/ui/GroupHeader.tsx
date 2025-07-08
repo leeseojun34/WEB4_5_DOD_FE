16ff9d0aea8765a2519d59728149ecbbd7415c2b
Suji <soojiya2003@gmail.com>
2025-07-08 15:00:11 +0900

    Design : icon 색상값 수정(currentColor) 및 태그박스 패딩값 수정

--- src/components/ui/GroupHeader.tsx
+++ src/components/ui/GroupHeader.tsx
@@ -1,12 +1,12 @@
 import { ChartNoAxesColumn, ChevronRight, Link2 } from "lucide-react";
 
-import userIcon from "../../assets/icon/user_icon.svg";
-import Image from "next/image";
+import UserIcon from "../../assets/icon/user_icon.svg";
+
 import HeaderTop from "../ui/HeaderTop";
 
 const fontStyleIntro = "text-sm text-[color:var(--color-white)] font-normal";
 const tagStyle =
-  "flex items-center justify-center gap-1 bg-[color:var(--color-white-30)] rounded-sm px-2 py-0.5 text-xs text-white cursor-pointer";
+  "flex items-center justify-center gap-1 bg-[color:var(--color-white-30)] rounded-sm pl-2 pr-0.5 py-0.5 text-xs text-white cursor-pointer";
 
 type GroupHeaderProps = {
   children: React.ReactNode;
@@ -28,15 +28,10 @@ const GroupHeader = ({ children, topIcon }: GroupHeaderProps) => {
         {/*  소개문구 */}
         <p className={fontStyleIntro}>던전앤 파이터 커플 놀이 하는 날</p>
         <div className={tagStyle}>
-          <Image
-            src={userIcon}
-            alt="userIcon"
-            width={16}
-            height={16}
-            className="filter invert"
-          />
+          <UserIcon className="text-[var(--color-white)]" />
+
           {/* 인원수 */}
-          <span>2</span>
+          <span className="pr-1.5">2</span>
         </div>
         {/* 방장 권한이 있는 경우에만 보이는 부분 */}
         <div className="flex items-center justify-center gap-4">
