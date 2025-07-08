29e62f23d1b0fd2e5a47433a9b8d3a517b28685f
Suji <soojiya2003@gmail.com>
2025-07-08 14:42:47 +0900

    Chore : SVGR 로더 추가

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+import { NextConfig } from "next";
+import { join } from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: { and: [/\.(js|ts)x?$/] },
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
