468d34c069a99a606e1ce213a155ddd564749a8a
Jungyu Park <parkjungyu09@gmail.com>
2025-07-08 21:50:29 +0900

    Style: 달력 공통 컴포넌트 디자인 수정

--- src/components/feature/Calender.tsx
+++ src/components/feature/Calender.tsx
@@ -5,37 +5,38 @@ import "react-day-picker/style.css";
 import { ko } from "date-fns/locale";
 
 export const Calendar = () => {
-  const [selected, setSelected] = useState<Date>();
+  const [selected, setSelected] = useState<Date[]>([]);
   const defaultClassNames = getDefaultClassNames();
+
+  console.log(defaultClassNames);
 
   return (
     <DayPicker
+      ISOWeek
       locale={ko}
       mode="multiple"
-      // selected={selected}
-      // onSelect={setSelected}
+      required={true}
+      selected={selected}
+      onSelect={setSelected}
       navLayout="around"
       showOutsideDays={true}
       formatters={{
         formatCaption: (month) =>
           `${month.getFullYear()}년 ${month.getMonth() + 1}월`,
       }}
-  
       classNames={{
-        day: `rounded-full`,
-        today: `border border-[color:var(--color-primary-400)] text-[color:var(--color-primary-400)]`,
+        root: `${defaultClassNames.root} w-[335px] h-full rounded-[20px] bg-[color:var(--color-white)] px-4 py-3 flex justify-center`,
+        day: `rounded-full leading-none`,
+        caption_label: `font-semibold flex justify-center items-center text-sm`,
+        today: `outline outline-[color:var(--color-primary-400)] text-[color:var(--color-primary-400)] box-border w-6 h-6`,
         weekday: `text-[color:var(--color-gray-placeholder)] font-medium text-sm`,
         selected: `bg-[color:var(--color-primary-400)] text-[color:var(--color-white)]`,
-        root: `${defaultClassNames.root} w-[335px] h-full rounded-lg bg-[color:var(--color-white)] py-3`,
-        chevron: `${defaultClassNames.chevron} fill-[color:var(--color-black)]! `,
+        chevron: `${defaultClassNames.chevron} fill-[color:var(--color-black)]! w-[18px] h-[18px]`,
         outside: `text-[color:var(--color-gray-placeholder)]`,
-        month_grid: `border-separate border-spacing-[12px]`,
-        day_button: `w-6 h-6 text-sm font-medium leading-none`,
+        month_grid: `border-separate border-spacing-4`,
+        day_button: `w-6 h-6 text-sm font-medium`,
       }}
-
-      //   footer={
-      //     selected ? `Selected: ${selected.toLocaleDateString()}` : "Pick a day."
-      //   }
+      // footer={selected ? `Selected: ${selected}` : "Pick a day."}
     />
   );
 };
