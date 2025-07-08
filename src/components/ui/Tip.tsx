559fa98915c1aa40d2dbb400efdf2d0090cf4c63
Suji <soojiya2003@gmail.com>
2025-07-08 12:38:08 +0900

    Desgin : line height 변경 (tailwind 기분 맞춤)

--- src/components/ui/Tip.tsx
+++ src/components/ui/Tip.tsx
@@ -1,6 +1,6 @@
 const Tip = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="w-full flex flex-col px-5 py-4 gap-2 text-[color:var(--color-primary-400)] bg-[color:var(--color-primary-100)] rounded-lg text-xs leading-[21px]">
+    <div className="w-full flex flex-col px-5 py-4 gap-2 text-[color:var(--color-primary-400)] bg-[color:var(--color-primary-100)] rounded-lg text-xs leading-5">
       <p className="font-medium">이때, Tip</p>
       <p className="font-normal">{children}</p>
     </div>
