a7065c24cb5bd4a6d0441dd20f8a536130c8d6a6
snowari <70631889+snowari@users.noreply.github.com>
2025-07-08 22:39:01 +0900

    feat : 카카오 지도 api 연결 및 컴포넌트 추가, 커스텀 마커 추가

--- src/components/landing/Landing.tsx
+++ src/components/landing/Landing.tsx
@@ -1,9 +1,14 @@
 "use client";
 
+import Map from "../feature/Map";
+
 const Landing = () => {
   return (
     <>
       <div>Landing</div>
+      <div className="w-375 mx-10">
+        <Map />
+      </div>
     </>
   );
 };
