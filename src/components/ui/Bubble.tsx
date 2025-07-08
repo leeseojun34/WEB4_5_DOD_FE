cf6acccf75022089ef1096c140e569506ed1d299
Jungyu Park <parkjungyu09@gmail.com>
2025-07-08 12:41:29 +0900

    Style: 말풍선 animate bounce 효과 추가

--- src/components/ui/Bubble.tsx
+++ src/components/ui/Bubble.tsx
@@ -10,13 +10,17 @@ export const Bubble = ({
   className = "",
 }: BubbleProps) => {
   const baseStyle =
-    "text-center w-[280px] px-6 py-6 text-[color:var(--color-black)] bg-[color:var(--color-white)] rounded-lg";
+    "text-center w-[280px] px-6 py-6 text-[color:var(--color-black)] bg-[color:var(--color-white)] rounded-lg ";
   const sizeStyles = {
     sm: "text-xs",
     lg: "text-base",
   };
   return (
-    <div className="flex flex-col items-center drop-shadow-[0_8px_16px_rgba(27,33,44,0.12)]">
+    <div
+      className="flex flex-col items-center drop-shadow-[0_8px_16px_rgba(27,33,44,0.12)] animate-bounce"
+      // style={{ animationDuration: "100ms" }}
+      // 개 촐싹버전
+    >
       <div className={`${baseStyle} ${sizeStyles[size]} ${className}`}>
         {children}
       </div>
