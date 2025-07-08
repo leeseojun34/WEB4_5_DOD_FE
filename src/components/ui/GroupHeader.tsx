7a0ce5709013bd167eb8383bf78209e8691c6409
Suji <soojiya2003@gmail.com>
2025-07-08 11:47:37 +0900

    Design : userIcon 색상값 변경 및 기타 아이콘 사이즈 수정

--- src/components/ui/GroupHeader.tsx
+++ src/components/ui/GroupHeader.tsx
@@ -1,11 +1,12 @@
 import { ChartNoAxesColumn, ChevronRight, Link2 } from "lucide-react";
 
 import userIcon from "../../assets/icon/user_icon.svg";
+import Image from "next/image";
 import HeaderTop from "../ui/HeaderTop";
 
-const fontStyleIntro = "text-medium text-[color:var(--color-white)]";
+const fontStyleIntro = "text-sm text-[color:var(--color-white)] font-normal";
 const tagStyle =
-  "flex items-center justify-center gap-1 bg-[color:var(--color-white-90)] rounded-sm px-2 py-0.5 text-xs text-white cursor-pointer";
+  "flex items-center justify-center gap-1 bg-[color:var(--color-white-30)] rounded-sm px-2 py-0.5 text-xs text-white cursor-pointer";
 
 const GroupHeader = () => {
   return (
@@ -17,20 +18,26 @@ const GroupHeader = () => {
         </HeaderTop>
         <p className={fontStyleIntro}>던전앤 파이터 커플 놀이 하는 날</p>
         <div className={tagStyle}>
-          <img src={userIcon} alt="userIcon" />
+          <Image
+            src={userIcon}
+            alt="userIcon"
+            width={16}
+            height={16}
+            className="filter invert"
+          />
           <span>2</span>
         </div>
         {/* 방장 권한이 있는 경우에만 보이는 부분 */}
         <div className="flex items-center justify-center gap-4">
           <div className={tagStyle}>
-            <ChartNoAxesColumn size={16} />
+            <ChartNoAxesColumn size={14} strokeWidth={3} />
             <span>통계</span>
-            <ChevronRight color="[color:var(--color-white)]" size={16} />
+            <ChevronRight color="var(--color-white)" size={14} />
           </div>
           <div className={tagStyle}>
-            <Link2 size={16} />
+            <Link2 size={12} strokeWidth={2} />
             <span>초대</span>
-            <ChevronRight color="[color:var(--color-white)]" size={16} />
+            <ChevronRight color="var(--color-white)" size={14} />
           </div>
         </div>
       </div>
