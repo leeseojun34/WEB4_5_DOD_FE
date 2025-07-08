abf3fa67ef255bee46b41ae0fdbb07bbf8746189
Suji <soojiya2003@gmail.com>
2025-07-08 16:31:49 +0900

    Design : 클릭핸들러 추가

--- src/components/ui/GroupHeader.tsx
+++ src/components/ui/GroupHeader.tsx
@@ -1,12 +1,7 @@
 import { ChartNoAxesColumn, ChevronRight, Link2 } from "lucide-react";
-
 import UserIcon from "../../assets/icon/user_icon.svg";
-
 import HeaderTop from "../ui/HeaderTop";
-
-const fontStyleIntro = "text-sm text-[color:var(--color-white)] font-normal";
-const tagStyle =
-  "flex items-center justify-center gap-1 bg-[color:var(--color-white-30)] rounded-sm pl-2 pr-0.5 py-0.5 text-xs text-white cursor-pointer";
+import Link from "next/link";
 
 type GroupHeaderProps = {
   children?: React.ReactNode;
@@ -14,13 +9,19 @@ type GroupHeaderProps = {
   topIcon?: "pen" | "ellipsis" | "";
 };
 
+const fontStyleIntro = "text-sm text-[color:var(--color-white)] font-normal";
+const tagStyle =
+  "flex items-center justify-center gap-1 bg-[color:var(--color-white-30)] rounded-sm pl-2 pr-0.5 py-0.5 text-xs text-white cursor-pointer";
+
 // children ❌ 그룹 정보 받아와서 사용하도록 추후 수정
 
 const GroupHeader = ({ children, topIcon = "" }: GroupHeaderProps) => {
+  const moveToInvite = () => {
+    // 초대 링크
+  };
   return (
     <>
-      {/*  그룹 정보 : 그룹명, 소개문구, 인원수  */}
-      {/* group 헤더 */}
+      {/* group 헤더,  그룹 정보 : 그룹명, 소개문구, 인원수*/}
       <div className="w-full flex flex-col items-center justify-center pb-5 bg-[color:var(--color-primary-400)] gap-4">
         <HeaderTop fontColor="white" backward={true} icon={topIcon}>
           {/* 그룹명 */}
@@ -36,12 +37,15 @@ const GroupHeader = ({ children, topIcon = "" }: GroupHeaderProps) => {
         </div>
         {/* 방장 권한이 있는 경우에만 보이는 부분 */}
         <div className="flex items-center justify-center gap-4">
-          <div className={tagStyle}>
-            <ChartNoAxesColumn size={14} strokeWidth={3} />
-            <span>통계</span>
-            <ChevronRight color="var(--color-white)" size={14} />
-          </div>
-          <div className={tagStyle}>
+          <Link href="/statistics">
+            <div className={tagStyle}>
+              <ChartNoAxesColumn size={14} strokeWidth={3} />
+              <span>통계</span>
+              <ChevronRight color="var(--color-white)" size={14} />
+            </div>
+          </Link>
+
+          <div className={tagStyle} onClick={moveToInvite}>
             <Link2 size={12} strokeWidth={2} />
             <span>초대</span>
             <ChevronRight color="var(--color-white)" size={14} />
