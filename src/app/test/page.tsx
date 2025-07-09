7b9f7a9778a54d00952334373495fe04347c2ebb
Jungyu Park <parkjungyu09@gmail.com>
2025-07-09 10:31:50 +0900

    Feat: Calendar 컴포넌트 날짜 선택 상태 외부 관리 지원

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,6 +1,12 @@
-import { Calendar } from "@/components/feature/Calender";
+"use client";
 
-export default function page() {
+import { Calendar } from "@/components/feature/Calender";
+import { Button } from "@/components/ui/Button";
+import { useState } from "react";
+
+export default function Page() {
+  const [selected, setSelected] = useState<Date[] | undefined>([]);
+
   const answeredDays = [
     new Date("2025-07-08"),
     new Date("2025-07-09"),
@@ -8,8 +14,13 @@ export default function page() {
   ];
   return (
     <div className="flex min-h-screen justify-center items-center bg-black">
-      <div className="w-40">
-        <Calendar isCompact={true} />
+      <div className="w-[335px] flex flex-col gap-12">
+        <Calendar
+          isCompact={true}
+          selected={selected}
+          setSelected={setSelected}
+        />
+        <Button onClick={() => console.log(selected)}>제출</Button>
       </div>
     </div>
   );
--- src/components/feature/Calender.tsx
+++ src/components/feature/Calender.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import { Dispatch, SetStateAction, useState } from "react";
 import {
   CalendarDay,
   DayPicker,
@@ -12,22 +12,25 @@ import { IoChevronDown, IoChevronUp } from "react-icons/io5";
 import { ChevronLeft, ChevronRight } from "lucide-react";
 
 interface CalendarProps {
-  isCompact: boolean;
+  isCompact?: boolean;
   events?: Date[];
+  selected: Date[] | undefined;
+  setSelected: Dispatch<SetStateAction<Date[] | undefined>>;
 }
 
-export const Calendar = ({ isCompact, events }: CalendarProps) => {
-  const [selected, setSelected] = useState<Date[] | undefined>([]);
+export const Calendar = ({
+  isCompact = false,
+  events,
+  selected,
+  setSelected,
+}: CalendarProps) => {
   const [expanded, setExpanded] = useState(false);
   const defaultClassNames = getDefaultClassNames();
 
-  console.log(defaultClassNames);
   const today = new Date();
-  console.log(today.getDate());
 
   const startOfWeek = new Date(today);
 
-  console.log(startOfWeek);
   startOfWeek.setDate(today.getDate() - today.getDay() + 1);
   const weekDates = Array.from({ length: 7 }).map((_, i) => {
     const d = new Date(startOfWeek);
@@ -93,20 +96,6 @@ export const Calendar = ({ isCompact, events }: CalendarProps) => {
             );
           },
         }}
-        // components={{
-        //   NextMonthButton: () => (
-        //     <button>
-        //       <ChevronRight
-        //         className={`${defaultClassNames.button_next} w-4! h-4! stroke-1 translate-y-[3px]`}
-        //       />
-        //     </button>
-        //   ),
-        //   PreviousMonthButton: () => (
-        //     <ChevronLeft
-        //       className={` ${defaultClassNames.button_previous} w-4! h-4! stroke-1 translate-x-[22px] translate-y-[3px]`}
-        //     />
-        //   ),
-        // }}
         classNames={{
           root: `${defaultClassNames.root} w-[335px] h-[full] rounded-[20px] bg-[color:var(--color-white)] p-4 flex flex-col justify-center items-center`,
           day: `rounded-full leading-none`,
@@ -114,7 +103,6 @@ export const Calendar = ({ isCompact, events }: CalendarProps) => {
           today: `text-[color:var(--color-primary-400)] box-border w-6 h-6`,
           weekday: `text-[color:var(--color-gray-placeholder)] font-light text-xs`,
           selected: `bg-[color:var(--color-primary-400)] text-[color:var(--color-white)]`,
-          // chevron: `${defaultClassNames.chevron} fill-[color:var(--color-black)]! w-[18px] h-[18px]`,
           outside: `text-[color:var(--color-gray-placeholder)]`,
           month_grid: `border-separate border-spacing-x-4 ${
             expanded ? "border-spacing-y-4" : "border-spacing-y-1"
