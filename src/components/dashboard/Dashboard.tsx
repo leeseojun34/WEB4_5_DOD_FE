5315f460db5de3116c578aa360ba5d33ee2a13c8
Jungyu Park <parkjungyu09@gmail.com>
2025-07-09 17:40:18 +0900

    Style: Dashboard 달력 추가

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -1,3 +1,4 @@
+import { Calendar } from "../feature/calendar/Calender";
 import { Banner } from "./Banner";
 import { MyGroupSection } from "./MyGroupSection";
 import { MyScheduleSection } from "./MyScheduleSection";
@@ -7,8 +8,7 @@ const Dashboard = () => {
     <div className="w-full flex min-h-screen justify-center bg-[color:var(--color-gray-background)] py-4 px-5">
       <div className="w-full max-w-185 flex flex-col items-center gap-5 px-5">
         <Banner />
-        {/* 달력 들어갈 위치 */}
-        <div className="rounded-[20px] bg-[color:var(--color-white)] w-full h-[146px]"></div>
+        <Calendar isCompact={true} />
         <MyScheduleSection />
         <MyGroupSection />
       </div>
