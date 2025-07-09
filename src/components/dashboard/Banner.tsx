88ca0f9920b0ebfda722f21b93bf21b99e401c87
Jungyu Park <parkjungyu09@gmail.com>
2025-07-10 01:10:17 +0900

    Style: banner 이미지 수정 및 이미지 크기 조정

--- src/components/dashboard/Banner.tsx
+++ src/components/dashboard/Banner.tsx
@@ -4,12 +4,12 @@ import Image from "next/image";
 
 export const Banner = () => {
   return (
-    <div className="flex w-full h-30 md:h-[178px] bg-gradient-to-r from-[#8AB6FF] to-[#5291F4] rounded-[20px] px-4 md:px-10 justify-between items-center">
+    <div className="flex w-full h-30 md:h-[178px] bg-gradient-to-r from-[#8AB6FF] to-[#5291F4] rounded-[20px] px-6 md:px-10 justify-between items-center">
       <Image
         src={bannerImg}
         alt="배너 이미지"
         priority
-        className="w-24 h-24 md:w-[242px] md:h-[182px]"
+        className="w-28 h-28 md:w-[170px] md:h-[170px]"
       />
       <div className="flex flex-col h-auto justify-between py-5">
         <div className="font-[TTTogether] text-[color:var(--color-white)] font-regular text-xl md:text-[40px] text-end">
