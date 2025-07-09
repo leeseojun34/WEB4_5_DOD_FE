749325bfea8220f329a9505b1bbf5910ae3fd683
Jungyu Park <parkjungyu09@gmail.com>
2025-07-09 17:46:14 +0900

    Stlye: Dashboard padding 수치 조정

--- src/components/dashboard/Dashboard.tsx
+++ src/components/dashboard/Dashboard.tsx
@@ -6,7 +6,7 @@ import { MyScheduleSection } from "./MyScheduleSection";
 const Dashboard = () => {
   return (
     <div className="w-full flex min-h-screen justify-center bg-[color:var(--color-gray-background)] py-4 px-5">
-      <div className="w-full max-w-185 flex flex-col items-center gap-5 px-5">
+      <div className="w-full max-w-185 flex flex-col items-center gap-5">
         <Banner />
         <Calendar isCompact={true} />
         <MyScheduleSection />
