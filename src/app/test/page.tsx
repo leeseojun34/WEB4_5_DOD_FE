1471ef7ed8196c82875642c72df0884365119078
Jungyu Park <parkjungyu09@gmail.com>
2025-07-10 01:02:47 +0900

    Feat: 스케쥴 아이템 컴포넌트 구현

---
+++ src/app/test/page.tsx
@@ -0,0 +1,11 @@
+import ScheduleItem from "@/components/ui/ScheduleItem";
+
+export default function page() {
+  return (
+    <>
+      <div className="min-h-screen w-full flex justify-center items-center px-5 bg-[color:var(--color-gray-background)]">
+        <ScheduleItem />
+      </div>
+    </>
+  );
+}
---
+++ src/components/ui/ScheduleItem.tsx
@@ -0,0 +1,43 @@
+import { EllipsisVertical } from "lucide-react";
+import NameTag from "./NameTag";
+
+interface ScheduleItemProps {
+  name: string;
+  onoffline: "온라인" | "오프라인";
+  time: string;
+  members: string[];
+}
+const ScheduleItem = ({
+  name,
+  onoffline,
+  time,
+  members,
+}: ScheduleItemProps) => {
+  return (
+    <div className="min-w-[335px] max-w-185 w-full h-auto p-4 rounded-lg bg-[color:var(--color-white)] shadow-[--shadow-common] gap-2 flex flex-col">
+      <div className="flex justify-between">
+        <div className="flex gap-3">
+          <p className="text-[color:var(--color-gray)] text-xs font-medium">
+            {name}
+          </p>
+          <p className="text-[color:var(--color-primary-400)] text-xs font-regular">
+            {onoffline}
+          </p>
+        </div>
+        <button>
+          <EllipsisVertical className="w-[18px] h-[18px] text-[color:var(--color-black)]" />
+        </button>
+      </div>
+      <div className="text-sm font-medium text-[color:var(--color-black)]">
+        {time}
+      </div>
+      <div className="flex gap-1">
+        {members.map((member, i) => (
+          <NameTag name={member} key={i} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ScheduleItem;
