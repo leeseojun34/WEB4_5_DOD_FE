3b6718cd1d531eaaaa690ad811aa70fb851ebee3
snowari <70631889+snowari@users.noreply.github.com>
2025-07-10 12:35:45 +0900

    ✨ Feat : 투표 결과 페이지 퍼블리싱

--- src/components/ui/OptionBox.tsx
+++ src/components/ui/OptionBox.tsx
@@ -1,11 +1,20 @@
 interface OptionBoxProps {
   isSelected: boolean;
   children: React.ReactNode;
+  isPointer?: boolean;
 }
 
-export const OptionBox = ({ isSelected, children }: OptionBoxProps) => {
-  const baseStyle =
-    "flex flex-col items-center p-4 gap-2 w-full h-auto rounded-lg border cursor-pointer hover:border-[var(--color-primary-400)] hover:bg-[var(--color-primary-100)] transition duration-300";
+export const OptionBox = ({
+  isSelected,
+  children,
+
+  isPointer = true,
+}: OptionBoxProps) => {
+  const baseStyle = `
+    flex flex-col items-center p-4 gap-2 w-full h-auto rounded-lg border
+    ${isPointer ? "cursor-pointer" : "cursor-default"}
+    hover:border-[var(--color-primary-400)] hover:bg-[var(--color-primary-100)] transition duration-300
+  `;
   const selectedStyle = isSelected
     ? "border-[color:var(--color-primary-400)] bg-[color:var(--color-primary-100)]"
     : "border-black/10 text-[color:var(--color-gray)]";
