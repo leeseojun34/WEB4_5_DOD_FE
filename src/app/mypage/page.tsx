dfac6add10179a7410dd15b920ad8166db473705
Suji <soojiya2003@gmail.com>
2025-07-10 15:25:01 +0900

    Design : 요소간 폭 넓이 조정

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -35,8 +35,8 @@ const mypage = () => {
   };
 
   return (
-    <div className="w-full flex flex-col py-11">
-      <div className="flex flex-1 flex-col justify-between gap-27">
+    <div className="w-full flex flex-col py-8">
+      <div className="flex flex-1 flex-col justify-between gap-[4vh]">
         <div className="flex flex-col gap-8">
           <Profile
             editHandler={() => {
@@ -52,11 +52,11 @@ const mypage = () => {
             <ListBox clickHandler={calendarUpdateHandler}>캘린더 연동</ListBox>
           </div>
         </div>
-        <div className="flex justify-between items-center text-xs mx-23">
+        <div className="flex justify-center items-center text-xs gap-24">
           <span className="font-light text-[var(--color-gray-placeholder)] hover:text-[var(--color-primary-400)] cursor-pointer">
             계정탈퇴
           </span>
-          <span className="font-normal text-[var(--color-gray-placeholder)] hover:text-[var(--color-primary-400)] cursor-pointer">
+          <span className="font-light text-[var(--color-gray-placeholder)] hover:text-[var(--color-primary-400)] cursor-pointer">
             로그아웃
           </span>
         </div>
@@ -64,7 +64,7 @@ const mypage = () => {
 
       {sheetType === "name" && (
         <BottomSheet isOpen={isOpen} setIsOpen={setIsOpen} snapPoints={350}>
-          <div className="w-full flex flex-col px-5 gap-8">
+          <div className="w-full flex flex-col px-5 gap-8 pb-12">
             <div className="flex justify-between items-center px-5">
               <X className="invisible" />
               <span className="text-base font-medium mt-3">이름 수정</span>
@@ -74,7 +74,6 @@ const mypage = () => {
                 className="text-[var(--color-black)] cursor-pointer"
               />
             </div>
-
             <Input
               label="이름"
               value={text}
@@ -84,13 +83,15 @@ const mypage = () => {
               }
               placeholder="이름을 입력해주세요."
             />
-            <Button onClick={saveHandler}>저장하기</Button>
+            <div className="flex justify-between items-center">
+              <Button onClick={saveHandler}>저장하기</Button>
+            </div>
           </div>
         </BottomSheet>
       )}
       {sheetType === "time" && (
-        <BottomSheet isOpen={isOpen} setIsOpen={setIsOpen} snapPoints={800}>
-          <div className="w-full flex flex-col px-5 gap-8">
+        <BottomSheet isOpen={isOpen} setIsOpen={setIsOpen} snapPoints={900}>
+          <div className="w-full h-full flex flex-col gap-8 pb-12">
             <div className="flex justify-between items-center px-5">
               <X className="invisible" />
               <span className="text-base font-medium mt-3">
@@ -102,9 +103,12 @@ const mypage = () => {
                 className="text-[var(--color-black)] cursor-pointer"
               />
             </div>
-
-            <Schedule />
-            <Button onClick={saveHandler}>저장하기</Button>
+            <div className="w-full flex-1 overflow-y-auto ">
+              <Schedule />
+            </div>
+            <div className="flex justify-between items-center px-5">
+              <Button onClick={saveHandler}>저장하기</Button>
+            </div>
           </div>
         </BottomSheet>
       )}
@@ -116,8 +120,8 @@ const mypage = () => {
         //   역찾기
         // </BottomsheetSearchPlace>
         <BottomSheet isOpen={isOpen} setIsOpen={setIsOpen} snapPoints={400}>
-          <div className="w-full flex flex-col px-5 gap-8">
-            <div className="flex justify-between items-center px-5">
+          <div className="w-full flex flex-col px-5 gap-8 pb-12">
+            <div className="flex justify-between items-center ">
               <X className="invisible" />
               <span className="text-base font-medium mt-3">역찾기</span>
               <X
@@ -145,9 +149,9 @@ const mypage = () => {
                 </div>
               </div>
             </div>
-
-            <div></div>
-            <Button onClick={saveHandler}>저장하기</Button>
+            <div className="flex justify-between items-center">
+              <Button onClick={saveHandler}>저장하기</Button>
+            </div>
           </div>
         </BottomSheet>
       )}
