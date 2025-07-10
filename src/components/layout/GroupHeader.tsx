0e46b6776a1ed2233d43bfe98eecc741beda6771
Suji <soojiya2003@gmail.com>
2025-07-11 00:58:36 +0900

    Design : group 전용 헤더로 수정

--- src/components/layout/GroupHeader.tsx
+++ src/components/layout/GroupHeader.tsx
@@ -1,7 +1,15 @@
-import { ChartNoAxesColumn, ChevronRight, Link2 } from "lucide-react";
-import HeaderTop from "./HeaderTop";
+import {
+  ChartNoAxesColumn,
+  ChevronLeft,
+  ChevronRight,
+  Ellipsis,
+  Link2,
+  Plus,
+  SquarePen,
+} from "lucide-react";
 import Link from "next/link";
 import { BiUser } from "react-icons/bi";
+import { useRouter } from "next/navigation";
 
 type GroupHeaderProps = {
   groupName: string;
@@ -9,9 +17,13 @@ type GroupHeaderProps = {
   groupCount: number;
   clickToInvite: () => void;
   // 최상단 오른쪽 아이콘(pen, ellipsis)
-  topIcon?: "pen" | "ellipsis" | "";
+  backward?: boolean;
+  icon?: "pen" | "ellipsis" | "";
+  clickPenHandler?: () => void;
+  clickEllipsisHandler?: () => void;
 };
 
+const fontStyleWhite = "text-lg text-[color:var(--color-white)]";
 const fontStyleIntro = "text-sm text-[color:var(--color-white)] font-normal";
 const tagStyle =
   "flex items-center justify-center gap-1 bg-[color:var(--color-white-30)] rounded-sm pl-2 pr-0.5 py-0.5 text-xs text-white cursor-pointer";
@@ -21,16 +33,46 @@ const GroupHeader = ({
   groupIntroduction,
   groupCount,
   clickToInvite,
-  topIcon = "",
+  backward = true,
+  icon = "",
+  clickPenHandler,
+  clickEllipsisHandler,
 }: GroupHeaderProps) => {
+  const router = useRouter();
+  const handleBack = () => {
+    router.back();
+  };
   return (
     <>
       {/* group 헤더,  그룹 정보 : 그룹명, 소개문구, 인원수*/}
-      <div className="w-full flex flex-col items-center justify-center pb-5 bg-[color:var(--color-primary-400)] gap-4">
-        <HeaderTop fontColor="white" backward={true} icon={topIcon}>
-          {/* 그룹명 */}
-          {groupName}
-        </HeaderTop>
+      <div className="w-full flex flex-col items-center justify-center pb-5 pt-11 bg-[color:var(--color-primary-400)] gap-4">
+        <div className="w-full flex justify-between items-center">
+          <span onClick={handleBack} className="cursor-pointer">
+            <ChevronLeft
+              color={"var(--color-white)"}
+              size={20}
+              className={backward ? "" : "invisible"}
+            />
+          </span>
+          {/* 그룹 이름 */}
+          <span className={fontStyleWhite}>{groupName}</span>
+
+          {icon === "pen" && (
+            <span onClick={clickPenHandler} className="cursor-pointer">
+              <SquarePen color={"var(--color-white)"} size={16} />
+            </span>
+          )}
+          {icon === "ellipsis" && (
+            <span onClick={clickEllipsisHandler} className="cursor-pointer">
+              <Ellipsis color={"var(--color-white)"} size={16} />
+            </span>
+          )}
+          {icon === "" && (
+            <span>
+              <SquarePen size={16} className="invisible" />
+            </span>
+          )}
+        </div>
         {/*  소개문구 */}
         <p className={fontStyleIntro}>{groupIntroduction}</p>
         <div className={tagStyle}>
