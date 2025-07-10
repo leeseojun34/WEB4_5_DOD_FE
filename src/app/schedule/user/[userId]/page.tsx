66157cbd9c6009aaabbc4dab429b3f4c1359b9c1
Jungyu Park <parkjungyu09@gmail.com>
2025-07-10 09:46:50 +0900

    Style: 나의 이때어때 일정 페이지 퍼블리싱

--- src/app/schedule/user/[userId]/page.tsx
+++ src/app/schedule/user/[userId]/page.tsx
@@ -1,11 +1,29 @@
 import rabbitWriting from "@/assets/images/rabbit_writing.png";
+import ScheduleItem from "@/components/ui/ScheduleItem";
 import Image from "next/image";
 
 const UserSchedule = () => {
   return (
-    <div className="min-w-[375px] min-h-screen bg-[color:var(--color-gray-background)] px-5">
-      <Image src={rabbitWriting} alt="글쓰는 토끼 이미지" />
-      <div></div>
+    <div className="min-w-[375px] w-full max-w-185 min-h-screen bg-[color:var(--color-gray-background)] px-5 pt-[133px] mx-auto">
+      <Image
+        src={rabbitWriting}
+        alt="글쓰는 토끼 이미지"
+        className="w-[178px] h-[178px] ml-auto"
+      />
+      <div className="flex flex-col items-center gap-4">
+        <ScheduleItem
+          name="카츠오모이가는날"
+          type="온라인"
+          time="7월 4일 (금) 18:00 - 22:00"
+          members={["박준규", "박은서", "현혜주", "박상윤", "황수지"]}
+        />
+        <ScheduleItem
+          name="박준규 팬미팅"
+          type="오프라인"
+          time="7월 5일 (토) 12:00 - 22:00"
+          members={["박준규", "박은서", "현혜주", "박상윤", "황수지"]}
+        />
+      </div>
     </div>
   );
 };
