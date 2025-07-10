14ad364740cb95b220ebd0b10ae8d88d6e9172c3
Jungyu Park <parkjungyu09@gmail.com>
2025-07-11 01:50:56 +0900

    Feat: MeetingInfo (모임 정보) 컴포넌트 구현

---
+++ src/components/feature/meeting/MeetingInfo.tsx
@@ -0,0 +1,38 @@
+import { Clock } from "lucide-react";
+import { MdOutlineGroups } from "react-icons/md";
+
+interface MeetingInfoProps {
+  members: string[];
+  time: string;
+}
+const MeetingInfo = ({ members, time }: MeetingInfoProps) => {
+  return (
+    <div className="bg-[color:var(--color-white)] px-5 py-4 gap-4 rounded-lg flex flex-col shadow-[--shadow-common]">
+      <div className="flex w-full items-center gap-4">
+        <div>
+          <MdOutlineGroups className="text-[color:var(--color-black)] w-4 h-4" />
+        </div>
+        <div className="w-full">
+          <div className="text-[color:var(--color-primary-300)] text-xs">
+            모임 인원
+          </div>
+          <div className="text-[color:var(--color-black)] text-sm">
+            {members.join(", ")}
+          </div>
+        </div>
+      </div>
+      <div className="flex w-full items-center gap-4">
+        <div>
+          <Clock className="text-[color:var(--color-black)] w-4 h-4" />
+        </div>
+        <div className="w-full">
+          <div className="text-[color:var(--color-primary-300)] text-xs">
+            모임 시간
+          </div>
+          <div className="text-[color:var(--color-black)] text-sm">{time}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default MeetingInfo;
