d90d620777f96b133af08641d784336a3cd19757
Jungyu Park <parkjungyu09@gmail.com>
2025-07-11 17:19:25 +0900

    Refactor: 변경된 바텀시트에 맞게 로직 수정

--- src/components/feature/schedule/editSchedule/TimeEditBottomSheet.tsx
+++ src/components/feature/schedule/editSchedule/TimeEditBottomSheet.tsx
@@ -7,7 +7,7 @@ interface TimeEditBottomSheetProps {
   isOpen: boolean;
   setIsOpen: (open: boolean) => void;
   selectedDate: Date | undefined;
-  setSelectedDate: Dispatch<SetStateAction<Date | undefined>>
+  setSelectedDate: Dispatch<SetStateAction<Date | undefined>>;
   onComplete: () => void;
 }
 
@@ -19,24 +19,26 @@ const TimeEditBottomSheet = ({
   onComplete,
 }: TimeEditBottomSheetProps) => {
   return (
-    <BottomSheet isOpen={isOpen} setIsOpen={setIsOpen} snapPoints={605}>
-      <div className="w-[375px] flex flex-col items-center px-5 mx-auto pt-3 gap-8">
-        <div className="flex justify-between w-full">
-          <X className="invisible w-5 h-5" />
-          <div className="font-semibold text-base text-[color:var(--color-black)]">
-            모임 시간 수정
+    <BottomSheet isOpen={isOpen} setIsOpen={setIsOpen} snapPoints={[0.55]}>
+      {() => (
+        <div className="w-[375px] flex flex-col items-center px-5 mx-auto pt-3 gap-8">
+          <div className="flex justify-between w-full">
+            <X className="invisible w-5 h-5" />
+            <div className="font-semibold text-base text-[color:var(--color-black)]">
+              모임 시간 수정
+            </div>
+            <X
+              className="w-5 h-5 text-[color:var(--color-black)] cursor-pointer"
+              onClick={() => setIsOpen(false)}
+            />
           </div>
-          <X
-            className="w-5 h-5 text-[color:var(--color-black)] cursor-pointer"
-            onClick={() => setIsOpen(false)}
+          <TimeSelector
+            selectedDate={selectedDate}
+            setSelectedDate={setSelectedDate}
+            onComplete={onComplete}
           />
         </div>
-        <TimeSelector
-          selectedDate={selectedDate}
-          setSelectedDate={setSelectedDate}
-          onComplete={onComplete}
-        />
-      </div>
+      )}
     </BottomSheet>
   );
 };
