320188bda82ca1484ad6837662ebad77f541fbcc
Jungyu Park <parkjungyu09@gmail.com>
2025-07-11 22:27:43 +0900

    Style: 그룹 헤더 props 수정

--- src/app/meeting/coordinate/page.tsx
+++ src/app/meeting/coordinate/page.tsx
@@ -4,10 +4,16 @@ import CoordinateContent from "@/components/feature/meeting/coordinate/Coordinat
 import GroupHeader from "@/components/layout/GroupHeader";
 
 const CoordinatePage = () => {
+  
   return (
     <div className="min-w-[375px] w-full max-w-185 mx-auto relative">
       <div>
-        <GroupHeader />
+        <GroupHeader
+          groupName="박준규 팬미팅"
+          groupIntroduction="박준규 팬미팅에 오신 것을 환영합니다"
+          groupCount={6000}
+          clickToInvite={() => console.log("초대함")}
+        />
       </div>
       <CoordinateContent />
     </div>
