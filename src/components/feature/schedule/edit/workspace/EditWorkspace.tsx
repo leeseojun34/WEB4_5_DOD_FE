5087b201d7576e899ac023ffd7e6b3e77880a567
hxezu <hailey_apple@icloud.com>
2025-07-11 21:37:56 +0900

    💄 style: workspace item 컴포넌트 퍼블리싱

--- src/components/feature/schedule/edit/workspace/EditWorkspace.tsx
+++ src/components/feature/schedule/edit/workspace/EditWorkspace.tsx
@@ -1,12 +1,43 @@
+import HeaderTop from "@/components/layout/HeaderTop";
 import WorkspaceItem from "./WorkspaceItem";
 
 const EditWorkspace = () => {
   return (
-    <WorkspaceItem
-      type="github"
-      name="이때어때 레포지토리"
-      url="https://github.com/prgrms-web-devcourse-final-project/WEB4_5_DOD_FE"
-    />
+    <>
+      <HeaderTop icon="plus">워크스페이스 관리</HeaderTop>
+      <div className="pt-25 px-5 space-y-4">
+        <WorkspaceItem
+          type="miro"
+          name="이때어때 미로"
+          url="https://www.notion.so/Team07-21e15a01205480a49fc6d1e73f119a19"
+        />
+        <WorkspaceItem
+          type="github"
+          name="이때어때 레포지토리"
+          url="https://www.notion.so/Team07-21e15a01205480a49fc6d1e73f119a19"
+        />
+        <WorkspaceItem
+          type="figma"
+          name="이때어때 피그마"
+          url="https://www.notion.so/Team07-21e15a01205480a49fc6d1e73f119a19"
+        />
+        <WorkspaceItem
+          type="notion"
+          name="이때어때 노션"
+          url="https://www.notion.so/Team07-21e15a01205480a49fc6d1e73f119a19"
+        />
+        <WorkspaceItem
+          type="googledocs"
+          name="이때어때 구글 독스"
+          url="https://www.notion.so/Team07-21e15a01205480a49fc6d1e73f119a19"
+        />
+        <WorkspaceItem
+          type="canva"
+          name="이때어때 캔바"
+          url="https://www.notion.so/Team07-21e15a01205480a49fc6d1e73f119a19"
+        />
+      </div>
+    </>
   );
 };
 export default EditWorkspace;
--- src/components/feature/schedule/edit/workspace/WorkspaceItem.tsx
+++ src/components/feature/schedule/edit/workspace/WorkspaceItem.tsx
@@ -5,7 +5,7 @@ import canvaIcon from "@/assets/icon/canva_icon.svg";
 import googledocsIcon from "@/assets/icon/googledocs_icon.svg";
 import figmaIcon from "@/assets/icon/figma_icon.svg";
 import Image from "next/image";
-import Link from "next/link";
+import { Edit } from "lucide-react";
 
 interface WorkspaceItemProps {
   type: "github" | "notion" | "miro" | "figma" | "canva" | "googledocs";
@@ -25,16 +25,34 @@ const workspaceLogos = {
 const WorkspaceItem = ({ type, name, url }: WorkspaceItemProps) => {
   const logo = workspaceLogos[type];
   return (
-    <div className="flex flex-col">
-      <div className="flex">
-        {logo && (
-          <Image src={logo} alt={`${type} 로고`} width={24} height={24} />
-        )}
-        <span>{name}</span>
+    <>
+      <div className="min-w-[335px] max-w-185 w-full h-auto p-4 rounded-lg bg-[color:var(--color-white)] shadow-[var(--shadow-common)] gap-3 flex flex-col">
+        <div className="flex justify-between items-center">
+          <div className="flex gap-3">
+            {logo && (
+              <div className="w-5 h-5 relative flex-shrink-0">
+                <Image
+                  src={logo}
+                  alt={`${type} 로고`}
+                  fill
+                  className="object-contain"
+                />
+              </div>
+            )}
+            <span className="text-sm">{name}</span>
+          </div>
+          <Edit
+            size={14}
+            className="cursor-pointer text-[color:var(--color-gray)]"
+          />
+        </div>
+        <div className="bg-[color:var(--color-gray-background)] rounded-sm text-[color:var(--color-gray)] px-2 py-1 text-xs underline">
+          <a href={url} target="_blank" rel="noopener noreferrer">
+            {url}
+          </a>
+        </div>
       </div>
-
-      <Link href={url}>{url}</Link>
-    </div>
+    </>
   );
 };
 export default WorkspaceItem;
