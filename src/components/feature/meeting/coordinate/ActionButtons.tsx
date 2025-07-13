ac0bfb541519e4d4d6f2c0176335f9efc080b86e
Jungyu Park <parkjungyu09@gmail.com>
2025-07-13 22:02:08 +0900

    Feat: 시간 조율 페이지 결과 버튼 클릭 시 조율 결과 페이지로 링크 이동

--- src/components/feature/meeting/coordinate/ActionButtons.tsx
+++ src/components/feature/meeting/coordinate/ActionButtons.tsx
@@ -1,14 +1,21 @@
+"use client";
+
 import { Button } from "@/components/ui/Button";
+import { useRouter } from "next/navigation";
 
 interface ActionButtonsProps {
   className?: string;
 }
 
 const ActionButtons = ({ className = "" }: ActionButtonsProps) => {
+  const router = useRouter();
+  const navigate = () => {
+    router.push("/meeting/coordinate/timeresult");
+  };
   return (
     <div className={`flex gap-5 w-full ${className}`}>
-      <Button>내 시간표 확정</Button>
-      <Button state="disabled">결과 보기</Button>
+      <Button state="disabled">내 시간표 확정</Button>
+      <Button onClick={navigate}>결과 보기</Button>
     </div>
   );
 };
