c6de0439a442ef0b0bfb438f6c767277edc250c4
Jungyu Park <parkjungyu09@gmail.com>
2025-07-13 21:52:14 +0900

    Feat: 새로운 일정 생성 버튼 클릭 후 다음 버튼 눌렀을 시 일정 생성 페이지로 이동

--- src/app/group/[groupId]/schedule/create/select/page.tsx
+++ src/app/group/[groupId]/schedule/create/select/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { OptionBox } from "@/components/ui/OptionBox";
 import Image from "next/image";
 import newScheduleImg from "@/assets/images/rabbit_new_schedule.png";
@@ -5,8 +7,17 @@ import prevScheduleImg from "@/assets/images/rabbit_prev_schedule.png";
 import Tip from "@/components/ui/Tip";
 import { Button } from "@/components/ui/Button";
 import HeaderTop from "@/components/layout/HeaderTop";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 const GroupScheduleCreateSelectPage = () => {
+  const [selected, setSelected] = useState("/schedule/create");
+  const router = useRouter();
+
+  const handleNavigate = () => {
+    router.push(`${selected}`);
+  };
+
   return (
     <div className="min-w-[375px] w-full max-w-185 px-5 flex flex-col items-center gap-9 mx-auto pt-25">
       <HeaderTop />
@@ -15,18 +26,23 @@ const GroupScheduleCreateSelectPage = () => {
           일정 추가 방법을 선택해주세요
         </p>
         <div className="w-full flex gap-5">
-          <OptionBox isSelected>
-            <Image src={newScheduleImg} alt="새로운 일정 추가 이미지" />
-            <p className="font-medium text-[color:var(--color-black)] text-center">
-              새로운 일정 <br />
-              생성
-            </p>
-          </OptionBox>
+          <div
+            className="w-full"
+            onClick={() => setSelected("/schedule/create")}
+          >
+            <OptionBox isSelected>
+              <Image src={newScheduleImg} alt="새로운 일정 추가 이미지" />
+              <p className="font-medium text-[color:var(--color-black)] text-center">
+                새로운 일정 <br />
+                생성
+              </p>
+            </OptionBox>
+          </div>
           <OptionBox isSelected={false}>
             <Image src={prevScheduleImg} alt="기존 일정 추가 이미지" />
             <p className="font-medium text-[color:var(--color-black)] text-center">
               내 일정 <br />
-              생성
+              불러오기
             </p>
           </OptionBox>
         </div>
@@ -34,7 +50,7 @@ const GroupScheduleCreateSelectPage = () => {
       <Tip>그룹 내 멤버들과 새로운 일정을 만들어보아요</Tip>
       <div className="fixed w-full left-0 right-0 px-5 bottom-9">
         <div className="max-w-185 mx-auto">
-          <Button>다음</Button>
+          <Button onClick={handleNavigate}>다음</Button>
         </div>
       </div>
     </div>
