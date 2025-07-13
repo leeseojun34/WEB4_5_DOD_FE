ce60b3c65499dd5f2914aa294cff9ec5a340db0d
Jungyu Park <parkjungyu09@gmail.com>
2025-07-13 21:09:53 +0900

    Refactor: Dashboard 배너 전체 영역 클릭시 링크 이동으로 수정

--- src/components/dashboard/Banner.tsx
+++ src/components/dashboard/Banner.tsx
@@ -5,7 +5,10 @@ import Link from "next/link";
 
 export const Banner = () => {
   return (
-    <div className="flex w-full h-30 md:h-[178px] bg-gradient-to-r from-[#8AB6FF] to-[#5291F4] rounded-[20px] px-6 md:px-10 justify-between items-center">
+    <Link
+      href={"/meeting"}
+      className="flex w-full h-30 md:h-[178px] bg-gradient-to-r from-[#8AB6FF] to-[#5291F4] rounded-[20px] px-6 md:px-10 justify-between items-center"
+    >
       <Image
         src={bannerImg}
         alt="배너 이미지"
@@ -17,13 +20,10 @@ export const Banner = () => {
           내 보석함에서 <br />
           누구를 꺼내 볼까?
         </div>
-        <Link
-          href={"/meeting"}
-          className="flex text-xs md:text-xl text-[color:var(--color-white)] justify-end items-center cursor-pointer"
-        >
+        <div className="flex text-xs md:text-xl text-[color:var(--color-white)] justify-end items-center cursor-pointer">
           일정 만들러 가기 <ArrowRight className="w-[14px] h-4" />
-        </Link>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 };
