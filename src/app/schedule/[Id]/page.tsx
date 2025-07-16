b30a0d6855700815cae19f0ad04f11074bb114a8
Jungyu Park <parkjungyu09@gmail.com>
2025-07-16 16:35:08 +0900

    Feat: 일정 페이지 컴포넌트 분리

--- src/app/schedule/[Id]/page.tsx
+++ src/app/schedule/[Id]/page.tsx
@@ -1,59 +1,8 @@
 "use client";
 
-import MeetingInfo from "@/components/feature/MeetingInfo";
-import MeetingLocation from "@/components/feature/MeetingLocation";
-import OnlineMeetingRoom from "@/components/feature/OnlineMeetingRoom";
-import WorkSpace from "@/components/feature/WorkSpace";
-import Footer from "@/components/layout/Footer";
-import GroupHeader from "@/components/layout/GroupHeader";
-import Header from "@/components/layout/Header";
-import ShareButton from "@/components/ui/ShareButton";
+import ScheduleDetail from "@/components/feature/schedule/detail/ScheduleDetail";
 
-const ScheduleDetail = () => {
-  const isOnline = true;
-  return (
-    <div className="w-full bg-[color:var(--color-gray-background)] min-h-screen">
-      <div className="hidden sm:block">
-        <Header type="blue" />
-      </div>
-      <GroupHeader
-        groupName="대나무 행주"
-        groupIntroduction="안녕하세요 대나무행주입니다람쥐"
-        groupCount={6}
-        clickToInvite={() => console.log("초대함")}
-      />
-      <div className="min-w-[375px] w-full max-w-185 mx-auto pt-6 sm:pt-10">
-        <div className="flex flex-col px-5 gap-4">
-          <ShareButton
-            title="상세 일정 정보 공유하기"
-            description="확정된 일정 내용을 공유해보세요"
-          />
-          <MeetingInfo
-            members={["박준규", "카리나"]}
-            time="7월 5일 (금) 12:00 - 24:00"
-          />
-          {isOnline ? (
-            <OnlineMeetingRoom platform="zoom" name=" 박준규 팬미팅" />
-          ) : (
-            <MeetingLocation
-              location="강남역"
-              specificLocation="강남역 스타벅스"
-            />
-          )}
-
-          <WorkSpace
-            workspaces={[
-              { platform: "notion", name: "프론트엔드 기획서" },
-              { platform: "github", name: "이때 어때 레포지토리" },
-              { platform: "miro", name: "이때 어때 미로" },
-            ]}
-          />
-        </div>
-      </div>
-      <div className="sm:hidden">
-        <Footer />
-      </div>
-    </div>
-  );
+const ScheduleDetailPage = () => {
+  return <ScheduleDetail />;
 };
-export default ScheduleDetail;
+export default ScheduleDetailPage;
