5c1c4158e5f07fe9fd0c4ca1f1df2fa4758e355e
hxezu <hailey_apple@icloud.com>
2025-07-17 12:15:09 +0900

    ♻️ DropdownSmall 컴포넌트 리팩토링

--- src/components/ui/DropdownSmall.tsx
+++ src/components/ui/DropdownSmall.tsx
@@ -1,11 +1,11 @@
 import { useEffect, useRef } from "react";
 
 type DropdownSmallProps = {
-  children?: [React.ReactNode, React.ReactNode];
+  children?: React.ReactNode | [React.ReactNode, React.ReactNode];
   isOpen: boolean;
   onClose: () => void;
   onTopClick: () => void;
-  onBottomClick: () => void;
+  onBottomClick?: () => void;
 };
 
 const DropdownSmall = ({
@@ -16,7 +16,8 @@ const DropdownSmall = ({
   onBottomClick,
 }: DropdownSmallProps) => {
   const menuRef = useRef<HTMLDivElement>(null);
-  const [topChild, bottomChild] = children ?? [];
+  const topChild = Array.isArray(children) ? children[0] : null;
+  const bottomChild = Array.isArray(children) ? children[1] : children;
 
   useEffect(() => {
     function handleClickOutside(e: MouseEvent) {
@@ -35,25 +36,31 @@ const DropdownSmall = ({
     <>
       {isOpen && (
         <div ref={menuRef} className="w-fit relative z-10">
-          <div className="min-w-[92px] flex flex-col  bg-[color:var(--color-white)] text-[var(--color-black)] px-5 py-2  rounded-lg gap-4 text-xs font-semibold shadow-[box-shadow:var(--shadow-common)]">
-            <div
-              className="w-full text-center hover:text-[var(--color-primary-400)] cursor-pointer"
-              onClick={(e) => {
-                e.stopPropagation();
-                onTopClick();
-                onClose();
-              }}>
-              {topChild}
-            </div>
-            <div
-              className="w-full text-center hover:text-[var(--color-primary-400)] cursor-pointer"
-              onClick={(e) => {
-                e.stopPropagation();
-                onBottomClick();
-                onClose();
-              }}>
-              {bottomChild}
-            </div>
+          <div className="min-w-[92px] flex flex-col  bg-[color:var(--color-white)] text-[var(--color-black)] px-5 py-2  rounded-lg gap-4 text-xs font-medium shadow-[box-shadow:var(--shadow-common)]">
+            {topChild && (
+              <div
+                className="w-full text-center cursor-pointer hover:font-semibold"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  onTopClick();
+                  onClose();
+                }}
+              >
+                {topChild}
+              </div>
+            )}
+            {bottomChild && onBottomClick && (
+              <div
+                className="ww-full text-center text-[var(--color-red)] cursor-pointer hover:font-semibold"
+                onClick={(e) => {
+                  e.stopPropagation();
+                  onBottomClick();
+                  onClose();
+                }}
+              >
+                {bottomChild}
+              </div>
+            )}
           </div>
         </div>
       )}
