1637ad234f06c8543dbeb5d987029d4fd4b2f781
Suji <soojiya2003@gmail.com>
2025-07-17 16:36:17 +0900

    Feat : 이름 수정, 장소 등록, 장소 수정 기능 구현

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -6,47 +6,142 @@ import Profile from "@/components/mypage/Profile";
 import StationSheet from "@/components/mypage/StationSheet";
 import TimeSheet from "@/components/mypage/TimeSheet";
 import AlertBox from "@/components/ui/AlertBox";
-import { useRouter } from "next/navigation";
-import { ChangeEvent, useState } from "react";
+import { useUser } from "@/hooks/useUser";
+import { useAddFavoriteLocation, useCalendarSync, useDeactiveMutation, useLogoutMutation, useUpdateFavoriteLocation, useUpdateName } from "@/lib/api/authApi";
+import { axiosInstance } from "@/lib/api/axiosInstance";
+import { kakaoSearch } from "@/types/kakaoSearch";
+import { useMutation, useQuery, useQueryClient,  } from "@tanstack/react-query";
+// import { useRouter } from "next/navigation";
+import {  useEffect, useState } from "react";
+import toast from "react-hot-toast";
 
-type SheetType = "name" | "time" | "station" | "calendar";
+type SheetType = "name" | "time" | "station";
 
 function MyPage() {
+  // const router = useRouter();
+  const queryClient = useQueryClient();
+  const { data: user } = useUser();
+  // console.log(user.data)
+  
   const [isOpen, setIsOpen] = useState(false);
   const [sheetType, setSheetType] = useState<SheetType | null>(null);
-  const [text, setText] = useState("");
 
-  const router = useRouter();
+  const favoriteQuery = useQuery({
+    queryKey: ["favoriteLocation"],
+    queryFn: async () => {
+      const res = await axiosInstance.get("/favorite-location");
+      const list = res.data.data;
+      return list.length > 0
+        ? list[0]
+        : {stationName : "미등록"}
+    },
+   
+  });
 
+  const [name, setName] = useState<string>(() => user?.data.name ?? "사용자이름");
+  const [newName, setNewName] = useState("")
+  const [email, setEmail] = useState<string>(() => user?.data.email ?? "");
+  const [profile, setProfile] = useState<number>(user?.data.profileImageNumber)
+  const [myStation, setMyStation] = useState<string>(
+    () => favoriteQuery.data?.stationName
+  );
+  const [calendarSynced, setCalendarSynced] = useState(false)
+
+   useEffect(() => {
+    if (user?.data) {
+      setName(user.data.name);
+      setEmail(user.data.email);
+      setProfile(user.data.profileImageNumber)
+    }
+    
+   }, [user]);
+  
+    useEffect(() => {
+    if (favoriteQuery.data) {
+      setMyStation(favoriteQuery.data.stationName);
+      // console.log(favoriteQuery.data)
+    }
+  }, [favoriteQuery.data])
+  
+  
   const openSheet = (type: SheetType) => {
     setSheetType(type);
     setIsOpen(true);
   };
 
-  const handleNameChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setText(e.target.value);
-  };
-  const handleStationChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setText(e.target.value);
+
+
+  const updateName = useUpdateName();
+  const handleNameSave = () => {
+   updateName.mutate(newName)
+   if (updateName.isSuccess) {
+    setName(newName)
+   }
+ }
+
+  const addFavoriteLocation = useAddFavoriteLocation();
+  const updateFavoriteLocation = useUpdateFavoriteLocation();
+
+
+  const handleStationSave = (station: kakaoSearch) => {
+    const stationName = station.place_name;
+    const longitude = Number(station.x)
+    const latitude = Number(station.y)
+    const favoritePlaceId = Number(favoriteQuery.data?.favoriteLocationId)
+  if (favoritePlaceId) {
+    updateFavoriteLocation.mutate(
+      { favoritePlaceId, stationName, latitude, longitude },
+      {
+        onSuccess: (res) => {
+          const updated = res.data?.stationName || stationName;
+          setMyStation(updated);
+        },
+      }
+    );
+  } 
+  else {
+    addFavoriteLocation.mutate(
+      { stationName, latitude, longitude },
+      {
+        onSuccess: (res) => {
+          const createdName = res.data?.stationName || stationName;
+          setMyStation(createdName);
+        },
+      }
+    );
+  }
+  setIsOpen(false);
   };
 
-  const handleLeave = () => {
-    console.log("탈퇴누름");
-    router.push("/");
-  };
+  const calendarMutation = useCalendarSync();
+  const handleGoogleCalendar = () => {
+    calendarMutation.mutate();
+    setCalendarSynced((prev) => !prev)
+    
+  }
+  
+    //로그아웃
+  const logoutMutation = useLogoutMutation();
   const handleLogout = () => {
     console.log("로그아웃누름");
-    router.push("/");
+    logoutMutation.mutate();
+  };
+  //회원탈퇴
+  const deactivateMutation = useDeactiveMutation();
+  const handleLeave = () => {
+    console.log("탈퇴누름");
+    deactivateMutation.mutate();
   };
 
-  // 추가 저장 로직 필요
-  const saveHandler = () => setIsOpen(false);
+
+  
+
 
   return (
     <div className="w-full flex flex-col py-8">
       <div className="flex flex-1 flex-col justify-between gap-[4vh]">
         <div className="flex flex-col gap-8">
-          <Profile editHandler={() => openSheet("name")} />
+        <Profile name={name} email={email} profile={profile} editHandler={() => openSheet("name")} />
 
           <div className="flex flex-col gap-4">
             <ListBox buttonText="수정" clickHandler={() => openSheet("time")}>
@@ -54,11 +149,11 @@ function MyPage() {
             </ListBox>
             <ListBox
               buttonText="등록"
-              station="고덕역"
+              station={myStation || "미등록"}
               clickHandler={() => openSheet("station")}>
               내 주변역
             </ListBox>
-            <ListBox buttonText="" clickHandler={() => openSheet("calendar")}>
+            <ListBox onConnect={handleGoogleCalendar} isConnected={calendarSynced} >
               캘린더 연동
             </ListBox>
           </div>
@@ -90,38 +185,27 @@ function MyPage() {
         <NameSheet
           isOpen={isOpen}
           setIsOpen={setIsOpen}
-          text={text}
-          onChange={handleNameChange}
-          onSave={() => {
-            /* 이름 저장 로직 추가*/
-            saveHandler();
-          }}
+          text={newName}
+          onChange={e => setNewName(e.target.value)}
+          onSave={handleNameSave}
         />
       )}
 
       {/* 나의 가능한 시간 설정하기 */}
       {sheetType === "time" && (
-        <TimeSheet
+         <TimeSheet
           isOpen={isOpen}
           setIsOpen={setIsOpen}
-          onSave={() => {
-            /* 시간 저장 로직 추가 */
-            saveHandler();
-          }}
+          onSave={() => {/* 시간 저장 로직 */ setIsOpen(false);}}
         />
       )}
 
       {/* 주변역 등록하기 */}
       {sheetType === "station" && (
-        <StationSheet
+       <StationSheet
           isOpen={isOpen}
           setIsOpen={setIsOpen}
-          text={text}
-          onChange={handleStationChange}
-          onSave={() => {
-            // 지하철역 저장 로직 추가
-            saveHandler();
-          }}
+          onSave={handleStationSave}
         />
       )}
     </div>
