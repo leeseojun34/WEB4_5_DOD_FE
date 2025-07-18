8a792ac939ac0492037079ed34ecf67a2fa82e95
Jungyu Park <parkjungyu09@gmail.com>
2025-07-18 12:05:36 +0900

    Fix: 이미지 화질 깨짐 현상 제거(unoptimized)

--- src/components/feature/group/analytics/EmptyGroupAnalytics.tsx
+++ src/components/feature/group/analytics/EmptyGroupAnalytics.tsx
@@ -1,19 +1,24 @@
 import Image from "next/image";
 import EmptyGroupAnalyticsCard from "./EmptyGroupAnalyticsCard";
 import sadRabbitImg from "@/assets/images/rabbit_noschedule_sad.png";
+import { Button } from "@/components/ui/Button";
 
 const EmptyGroupAnalytics = () => {
   return (
-    <div className="w-full h-full flex flex-col justify-center items-center gap-8 mt-40">
-      <Image
-        src={sadRabbitImg}
-        alt="데이터가 없어 슬픈 토끼 이미지"
-        priority
-        width={222}
-        height={183}
-        className="w-55 h-55 sm:w-90 sm:h-90"
-      />
-      <EmptyGroupAnalyticsCard />
+    <div className="w-full h-full flex flex-col justify-center items-center gap-20 mt-40">
+      <div className="w-full h-full flex flex-col justify-center items-center gap-8">
+        <Image
+          src={sadRabbitImg}
+          alt="데이터가 없어 슬픈 토끼 이미지"
+          priority
+          width={222}
+          height={183}
+          unoptimized
+          className="w-55 h-55 sm:w-90 sm:h-90"
+        />
+        <EmptyGroupAnalyticsCard />
+      </div>
+      <Button>일정 만들러 가기</Button>
     </div>
   );
 };
