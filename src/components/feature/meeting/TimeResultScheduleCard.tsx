3abeedcb1debf28cd92db2988a66fa3881cc0aa7
SK <ygvbhy@naver.com>
2025-07-18 15:07:13 +0900

    Fix: 조율 결과 수정사항 롤백

--- src/components/feature/meeting/TimeResultScheduleCard.tsx
+++ src/components/feature/meeting/TimeResultScheduleCard.tsx
@@ -33,19 +33,15 @@ const TimeResultScheduleCard = ({
 
   return (
     <div className="flex flex-col gap-4">
-      {scheduleList.map((item, index) => {
-        if (index < 3) {
-          return (
-            <ScheduleCard
-              key={item.key}
-              variant="attendance"
-              totalCount={totalParticipants}
-              members={item.members}
-              time={item.time}
-            />
-          );
-        }
-      })}
+      {scheduleList.map((item) => (
+        <ScheduleCard
+          key={item.key}
+          variant="attendance"
+          totalCount={totalParticipants}
+          members={item.members}
+          time={item.time}
+        />
+      ))}
     </div>
   );
 };
