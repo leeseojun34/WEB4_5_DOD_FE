9670609e49e5b71b2fa59f62dd0ff7fc2c638ef9
hxezu <hailey_apple@icloud.com>
2025-07-20 16:57:55 +0900

    ✨ 그룹 일정 추가 옵션 페이지 라우트 수정

--- src/app/group/[groupId]/schedule/create/select/page.tsx
+++ src/app/group/[groupId]/schedule/create/select/page.tsx
@@ -14,52 +14,75 @@ import Header from "@/components/layout/Header";
 const GroupScheduleCreateSelectPage = () => {
   const [selected, setSelected] = useState("/schedule/create");
   const router = useRouter();
+  const userId = "GOOGLE_116827515645165286764";
 
   const handleNavigate = () => {
     router.push(`${selected}`);
   };
 
   return (
-    <div className="w-full">
+    <section>
       <div className="hidden sm:block">
         <Header />
       </div>
-      <div className="min-w-[375px] w-full max-w-185 px-5 flex flex-col items-center gap-9 mx-auto pt-25 sm:pt-40">
-        <HeaderTop />
-        <div className="flex flex-col w-full gap-8">
-          <p className="w-full text-xl font-bold text-[color:var(--color-black)] text-start">
-            일정 추가 방법을 선택해주세요
-          </p>
-          <div className="w-full flex gap-5">
-            <div
-              className="w-full"
-              onClick={() => setSelected("/schedule/create")}
-            >
-              <OptionBox isSelected>
-                <Image src={newScheduleImg} alt="새로운 일정 추가 이미지" />
-                <p className="font-medium text-[color:var(--color-black)] text-center">
-                  새로운 일정 <br />
-                  생성
-                </p>
-              </OptionBox>
+      <div className="w-full">
+        <div className="min-w-[375px] w-full max-w-185 px-5 flex flex-col items-center gap-9 mx-auto pt-25 sm:pt-40">
+          <HeaderTop />
+          <div className="flex flex-col w-full gap-8">
+            <p className="w-full text-xl font-bold text-[color:var(--color-black)] text-start">
+              일정 추가 방법을 선택해주세요
+            </p>
+            <div className="w-full flex gap-5">
+              <div
+                className="w-full"
+                onClick={() => setSelected("/schedule/create")}
+              >
+                <OptionBox isSelected={selected === "/schedule/create"}>
+                  <Image
+                    height={52}
+                    width={52}
+                    src={newScheduleImg}
+                    alt="새로운 일정 추가 이미지"
+                  />
+                  <p className="font-medium text-[color:var(--color-black)] text-center">
+                    새로운 일정 <br />
+                    생성
+                  </p>
+                </OptionBox>
+              </div>
+              <div
+                className="w-full"
+                onClick={() => setSelected(`/schedule/user/${userId}`)}
+              >
+                <OptionBox isSelected={selected === "load"}>
+                  <Image
+                    height={52}
+                    width={52}
+                    src={prevScheduleImg}
+                    alt="기존 일정 추가 이미지"
+                  />
+                  <p className="font-medium text-[color:var(--color-black)] text-center">
+                    내 일정 <br />
+                    불러오기
+                  </p>
+                </OptionBox>
+              </div>
             </div>
-            <OptionBox isSelected={false}>
-              <Image src={prevScheduleImg} alt="기존 일정 추가 이미지" />
-              <p className="font-medium text-[color:var(--color-black)] text-center">
-                내 일정 <br />
-                불러오기
-              </p>
-            </OptionBox>
           </div>
-        </div>
-        <Tip>그룹 내 멤버들과 새로운 일정을 만들어보아요</Tip>
-        <div className="fixed w-full left-0 right-0 px-5 bottom-9">
-          <div className="max-w-185 mx-auto">
-            <Button onClick={handleNavigate}>다음</Button>
+          {selected === "/schedule/create" ? (
+            <Tip>새로운 만남, 여기서부터 시작해요! 🗓️</Tip>
+          ) : (
+            <Tip>전에 만든 일정이 있다면 불러와서 바로 써보세요! 🙌</Tip>
+          )}
+
+          <div className="fixed w-full left-0 right-0 px-5 bottom-9">
+            <div className="max-w-185 mx-auto">
+              <Button onClick={handleNavigate}>다음</Button>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 export default GroupScheduleCreateSelectPage;
