812080d02a762ffd64352d4bf0f2fa00f8efe8bf
Jungyu Park <parkjungyu09@gmail.com>
2025-07-23 01:53:01 +0900

    Feat: motion div 적용

--- src/components/feature/group/detail/GroupContent.tsx
+++ src/components/feature/group/detail/GroupContent.tsx
@@ -4,6 +4,8 @@ import Link from "next/link";
 import ShareButton from "@/components/ui/ShareButton";
 import ScheduleCard from "@/components/ui/ScheduleCard";
 import { formatSchedule } from "@/app/utils/dateFormat";
+import { itemVariants, listVariants } from "../../schedule/motion";
+import { motion } from "framer-motion";
 
 interface Schedule {
   scheduleName: string;
@@ -23,8 +25,16 @@ interface GroupContentProps {
 
 const GroupContent = ({ groupId, schedules, groupRole }: GroupContentProps) => {
   return (
-    <div className="min-w-[375px] w-full max-w-185 flex flex-col min-h-screen mx-auto">
-      <div className="flex flex-col p-5 pt-4 w-full gap-4 flex-1">
+    <motion.div
+      variants={listVariants}
+      initial="hidden"
+      animate="visible"
+      className="min-w-[375px] w-full max-w-185 flex flex-col min-h-screen mx-auto"
+    >
+      <motion.div
+        variants={itemVariants}
+        className="flex flex-col p-5 pt-4 w-full gap-4 flex-1"
+      >
         <Link href={`/group/${groupId}/schedule/create/select`}>
           <ShareButton
             title="그룹 일정 생성하기"
@@ -34,21 +44,24 @@ const GroupContent = ({ groupId, schedules, groupRole }: GroupContentProps) => {
         </Link>
         <div className="flex flex-col gap-4">
           {schedules.map((schedule, index) => (
-            <ScheduleCard
-              key={index}
-              variant="event"
-              title={schedule.scheduleName}
-              meetingType={schedule.meetingType}
-              time={formatSchedule(schedule.startTime, schedule.endTime)}
-              members={schedule.memberNames}
-              scheduleId={schedule.scheduleId}
-              groupRole={groupRole}
-              
-            />
+            <motion.div
+              variants={itemVariants}
+              key={`${schedule.scheduleId}-${index}`}
+            >
+              <ScheduleCard
+                variant="event"
+                title={schedule.scheduleName}
+                meetingType={schedule.meetingType}
+                time={formatSchedule(schedule.startTime, schedule.endTime)}
+                members={schedule.memberNames}
+                scheduleId={schedule.scheduleId}
+                groupRole={groupRole}
+              />
+            </motion.div>
           ))}
         </div>
-      </div>
-    </div>
+      </motion.div>
+    </motion.div>
   );
 };
 
