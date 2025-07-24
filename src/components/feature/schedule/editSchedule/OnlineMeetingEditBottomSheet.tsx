35591ff37ecb6a570b7fa46f296f92fa521d895a
Jungyu Park <parkjungyu09@gmail.com>
2025-07-24 22:52:09 +0900

    Refactor: 온라인미팅수정 바텀시트 로직 분리

--- src/components/feature/schedule/editSchedule/OnlineMeetingEditBottomSheet.tsx
+++ src/components/feature/schedule/editSchedule/OnlineMeetingEditBottomSheet.tsx
@@ -7,13 +7,10 @@ import googleMeetIcon from "@/assets/icon/googlemeet_icon.svg";
 import discordIcon from "@/assets/icon/discord_icon.svg";
 import zepIcon from "@/assets/icon/zep_icon.svg";
 import Image from "next/image";
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent } from "react";
 import useMediaQuery from "../hooks/useMediaQuery";
 import BottomSheetHeader from "@/components/layout/BottomSheetHeader";
-import {
-  useCreateMeetingRoom,
-  useUpdateScheduleInfo,
-} from "@/lib/api/scheduleApi";
+import { PlatformType, useOnlineMeetingForm } from "./hooks/useOnlineMeetingForm";
 
 interface OnlineMeetingEditBottomSheetProps {
   isOpen: boolean;
@@ -21,61 +18,30 @@ interface OnlineMeetingEditBottomSheetProps {
   scheduleId: string;
 }
 
-type PlatformType = "ZOOM" | "GOOGLE_MEET" | "DISCORD" | "ZEP";
+const ICONMAP: Record<PlatformType, string> = {
+  ZOOM: zoomIcon,
+  GOOGLE_MEET: googleMeetIcon,
+  DISCORD: discordIcon,
+  ZEP: zepIcon,
+};
 
 const OnlineMeetingEditBottomSheet = ({
   isOpen,
   setIsOpen,
   scheduleId,
 }: OnlineMeetingEditBottomSheetProps) => {
-  const [selectedPlatform, setSelectedPlatform] = useState<PlatformType | null>(
-    null
-  );
-  const [inputValue, setInputValue] = useState("");
   const isMobile = useMediaQuery("(min-width: 640px)");
   const snapPoints = isMobile ? [0.5] : [0.8];
-  const createMeetingRoom = useCreateMeetingRoom();
-  const updateScheduleInfo = useUpdateScheduleInfo();
 
-  const ICONMAP: Record<PlatformType, string | null> = {
-    ZOOM: zoomIcon,
-    GOOGLE_MEET: googleMeetIcon,
-    DISCORD: discordIcon,
-    ZEP: zepIcon,
-  };
-
-  const handleChangePlatform = (p: PlatformType) => {
-    setSelectedPlatform(p);
-  };
-
-  const handleCreateMeetingRoom = () => {
-    createMeetingRoom.mutate(scheduleId);
-    setIsOpen(false);
-  };
-
-  console.log(typeof scheduleId);
-
-  const handleUpdateMeetingRoom = () => {
-    updateScheduleInfo.mutate({
-      scheduleId,
-      data: {
-        meetingPlatform: selectedPlatform as PlatformType,
-        platformUrl: inputValue,
-      },
-    });
-    setIsOpen(false);
-  };
-
-  const handleDeleteMeetingRoom = () => {
-    updateScheduleInfo.mutate({
-      scheduleId,
-      data: {
-        meetingPlatform: "NONE",
-        platformUrl: null,
-      },
-    });
-    setIsOpen(false);
-  };
+  const {
+    selectedPlatform,
+    inputValue,
+    handleChangePlatform,
+    handleCreateMeetingRoom,
+    handleUpdateMeetingRoom,
+    handleDeleteMeetingRoom,
+    setInputValue,
+  } = useOnlineMeetingForm(scheduleId, () => setIsOpen(false));
 
   return (
     <BottomSheet isOpen={isOpen} setIsOpen={setIsOpen} snapPoints={snapPoints}>
@@ -86,13 +52,13 @@ const OnlineMeetingEditBottomSheet = ({
             title="온라인 회의장 정하기"
           />
           <ShareButton
-            title="회의장이 없나요"
+            title="회의장이 없나요?"
             description="Zoom 회의장을 만들어드려요"
             mode="help"
             color="var(--color-primary-100)"
             borderColor="var(--color-primary-400)"
             onClick={handleCreateMeetingRoom}
-          ></ShareButton>
+          />
           <div className="flex flex-col gap-4 w-full">
             <div className="flex flex-col gap-5">
               <p className="font-medium text-[color:var(--color-black)] text-sm">
@@ -103,16 +69,16 @@ const OnlineMeetingEditBottomSheet = ({
                   <div
                     key={p}
                     className={`flex justify-center items-center w-10 h-10 rounded-lg cursor-pointer
-                    ${
-                      selectedPlatform === p
-                        ? "bg-[color:var(--color-muted)]"
-                        : ""
-                    }
-                    hover:bg-[color:var(--color-muted)]`}
+                      ${
+                        selectedPlatform === p
+                          ? "bg-[color:var(--color-muted)]"
+                          : ""
+                      }
+                      hover:bg-[color:var(--color-muted)]`}
                     onClick={() => handleChangePlatform(p)}
                   >
                     <Image
-                      src={ICONMAP[p]!}
+                      src={ICONMAP[p]}
                       alt={`${p} 아이콘`}
                       className="w-6 h-6"
                     />
@@ -121,7 +87,7 @@ const OnlineMeetingEditBottomSheet = ({
               </div>
             </div>
             <Input
-              fullWidth={true}
+              fullWidth
               placeholder="온라인 회의장 링크를 입력해주세요"
               value={inputValue}
               onChange={(e: ChangeEvent<HTMLInputElement>) =>
