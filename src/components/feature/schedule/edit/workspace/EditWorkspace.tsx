2b459588493f8ae5329bf80049c613e7ffd4523d
Jungyu Park <parkjungyu09@gmail.com>
2025-07-24 22:49:09 +0900

    Feat: 워크스페이스 수정 페이지 데이터 페칭 및 렌더링

--- src/components/feature/schedule/edit/workspace/EditWorkspace.tsx
+++ src/components/feature/schedule/edit/workspace/EditWorkspace.tsx
@@ -1,51 +1,67 @@
-import WorkspaceItem from "./WorkspaceItem";
+"use client";
 
-const EditWorkspace = ({
-  onEditClick,
-}: {
-  onEditClick: (data: { type: string; name: string; url: string }) => void;
-}) => {
+import { useGroupSchedule } from "@/lib/api/scheduleApi";
+import WorkspaceItem from "./WorkspaceItem";
+import { useParams } from "next/navigation";
+import WorkspaceSkeletonItem from "./WorkspaceSkeletonItem";
+import { motion } from "framer-motion";
+import WorkspaceBottomSheet from "./WorkspaceBottomSheet";
+import { useWorkspaceModal } from "./hooks/useWorkspaceModal";
+
+type WorkspacePlatformType =
+  | "GITHUB"
+  | "NOTION"
+  | "MIRO"
+  | "FIGMA"
+  | "CANVA"
+  | "GOOGLE_DOCS";
+
+interface WorkspaceType {
+  type: WorkspacePlatformType;
+  name: string;
+  url: string;
+  workspaceId: string;
+}
+
+const EditWorkspace = () => {
+  const params = useParams();
+  const id = params.id as string;
+  const { data, isPending } = useGroupSchedule(id);
+  const workspaces = data?.data?.workspaces;
+
+  const { isOpen, selectedWorkspace, openModal, closeModal } =
+    useWorkspaceModal();
+
   return (
     <>
       <div className="space-y-4">
-        <WorkspaceItem
-          type="miro"
-          name="이때어때 미로"
-          url="https://www.notion.so/Team07-21e15a01205480a49fc6d1e73f119a19"
-          onClick={onEditClick}
-        />
-        <WorkspaceItem
-          type="github"
-          name="이때어때 레포지토리"
-          url="https://www.notion.so/Team07-21e15a01205480a49fc6d1e73f119a19"
-          onClick={onEditClick}
-        />
-        <WorkspaceItem
-          type="figma"
-          name="이때어때 피그마"
-          url="https://www.notion.so/Team07-21e15a01205480a49fc6d1e73f119a19"
-          onClick={onEditClick}
-        />
-        <WorkspaceItem
-          type="notion"
-          name="이때어때 노션"
-          url="https://www.notion.so/Team07-21e15a01205480a49fc6d1e73f119a19"
-          onClick={onEditClick}
-        />
-        <WorkspaceItem
-          type="google_dos"
-          name="이때어때 구글 독스"
-          url="https://www.notion.so/Team07-21e15a01205480a49fc6d1e73f119a19"
-          onClick={onEditClick}
-        />
-        <WorkspaceItem
-          type="canva"
-          name="이때어때 캔바"
-          url="https://www.notion.so/Team07-21e15a01205480a49fc6d1e73f119a19"
-          onClick={onEditClick}
-        />
+        {isPending &&
+          Array.from({ length: 4 }).map((_, i) => (
+            <WorkspaceSkeletonItem key={i} />
+          ))}
+
+        {workspaces?.map((workspace: WorkspaceType) => (
+          <motion.div key={workspace.workspaceId}>
+            <WorkspaceItem
+              type={workspace.type}
+              name={workspace.name}
+              url={workspace.url}
+              onClick={() => openModal(workspace)}
+            />
+          </motion.div>
+        ))}
       </div>
+
+      {isOpen && selectedWorkspace && (
+        <WorkspaceBottomSheet
+          isOpen={isOpen}
+          setIsOpen={closeModal}
+          defaultValue={selectedWorkspace}
+          workspaceId={selectedWorkspace.workspaceId}
+        />
+      )}
     </>
   );
 };
+
 export default EditWorkspace;
