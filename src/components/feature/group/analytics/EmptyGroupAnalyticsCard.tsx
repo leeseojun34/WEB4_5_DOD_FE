fdaf196001bf64b450d0cab2d5f40b0ed12b76c3
Jungyu Park <parkjungyu09@gmail.com>
2025-07-25 20:38:23 +0900

    Feat: EmptyGroupAnalyticsCard 모션 애니메이션 적용

--- src/components/feature/group/analytics/EmptyGroupAnalyticsCard.tsx
+++ src/components/feature/group/analytics/EmptyGroupAnalyticsCard.tsx
@@ -2,11 +2,21 @@ import Image from "next/image";
 import pastelOrangeImg from "@/assets/images/profile_pastel_orange.png";
 import pastelPurpleImg from "@/assets/images/profile_pastel_purple.png";
 import pastelGreenImg from "@/assets/images/profile_pastel_green.png";
+import { motion } from "framer-motion";
+import {
+  itemVariants,
+  listVariants,
+} from "@/components/feature/schedule/motion";
 
 const EmptyGroupAnalyticsCard = () => {
   return (
-    <div className="w-full flex flex-col gap-5">
-      <div className="flex gap-4 items-center">
+    <motion.div
+      variants={listVariants}
+      initial="hidden"
+      animate="visible"
+      className="w-full flex flex-col gap-5"
+    >
+      <motion.div variants={itemVariants} className="flex gap-4 items-center">
         <Image
           src={pastelOrangeImg}
           alt="오렌지 토끼 캐릭터 이미지"
@@ -16,8 +26,8 @@ const EmptyGroupAnalyticsCard = () => {
         <span className="text-[color:var(--color-gray)]">
           멤버별 참여율을 볼 수 있어요
         </span>
-      </div>
-      <div className="flex gap-4 items-center">
+      </motion.div>
+      <motion.div variants={itemVariants} className="flex gap-4 items-center">
         <Image
           src={pastelPurpleImg}
           alt="보라색 토끼 캐릭터 이미지"
@@ -27,8 +37,8 @@ const EmptyGroupAnalyticsCard = () => {
         <span className="text-[color:var(--color-gray)]">
           요일별 모임 빈도수를 알려드려요
         </span>
-      </div>
-      <div className="flex gap-4 items-center">
+      </motion.div>
+      <motion.div variants={itemVariants} className="flex gap-4 items-center">
         <Image
           src={pastelGreenImg}
           alt="초록색 토끼 캐릭터 이미지"
@@ -38,8 +48,8 @@ const EmptyGroupAnalyticsCard = () => {
         <span className="text-[color:var(--color-gray)]">
           어디서 주로 만나는지 확인해요
         </span>
-      </div>
-    </div>
+      </motion.div>
+    </motion.div>
   );
 };
 
