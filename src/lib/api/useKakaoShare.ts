809253969c59531182577ed7d253c410bc79c003
Jungyu Park <parkjungyu09@gmail.com>
2025-07-25 23:28:47 +0900

    Refactor: 불필요한 콘솔 제거

--- src/lib/api/useKakaoShare.ts
+++ src/lib/api/useKakaoShare.ts
@@ -1,6 +1,5 @@
 export const useKakaoShare = () => {
   const shareWithTemplate = (description: string, url = "") => {
-    console.log(url);
     window.Kakao.Share.sendCustom({
       templateId: 122694,
       templateArgs: {
