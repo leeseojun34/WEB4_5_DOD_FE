184ea52aaba7f62aaab06fc25f1dd6270a3347ca
Jungyu Park <parkjungyu09@gmail.com>
2025-07-27 02:38:17 +0900

    Feat: 그룹원 페이지 디자인 변경 및 그룹 멤버 초대 기능 추가

--- src/components/feature/group/GroupMemberItem.tsx
+++ src/components/feature/group/GroupMemberItem.tsx
@@ -39,7 +39,7 @@ const GroupMemberItem = ({
   } = useGroupMemberActions(groupId);
   const profileImage = profileImages[profileNum];
   return (
-    <div className="flex items-center justify-between w-full hover:bg-[color:var(--color-gray-background)] px-4 py-2 rounded-lg">
+    <div className="flex items-center justify-between w-full hover:bg-[color:var(--color-primary-100)] px-1 py-1 rounded-lg">
       <div className="flex gap-3 items-center">
         <div className="relative">
           <Image src={profileImage} alt="유저 캐릭터" className="w-6 h-7" />
