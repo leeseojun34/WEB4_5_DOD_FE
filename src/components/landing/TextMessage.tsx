258564b47231eb1ea3328da0f840b1975050eab0
Jungyu Park <parkjungyu09@gmail.com>
2025-07-27 22:06:57 +0900

    Style: 아이폰 텍스트 메시지 구현 및 랜딩페이지 컴포넌트 분리

---
+++ src/components/landing/TextMessage.tsx
@@ -0,0 +1,41 @@
+import { profileImages } from "@/lib/profileImages";
+import Image from "next/image";
+
+const TextMessage = ({
+  text,
+  isMine,
+  characterIndex,
+}: {
+  text: string;
+  isMine: boolean;
+  characterIndex: number;
+}) => {
+  const profileImage = profileImages[characterIndex];
+  return (
+    <div
+      className={`w-53 transparent px-4 space-y-1 flex flex-col ${
+        isMine ? "items-end" : "items-start"
+      }`}
+    >
+      {!isMine && (
+        <Image
+          src={profileImage}
+          alt={`프로필 이미지${characterIndex}`}
+          width={21}
+          height={23}
+        />
+      )}
+      <div
+        className={`${
+          isMine
+            ? "text-[color:var(--color-black)] bg-[color:var(--color-gray-background)] rounded-[8px] rounded-tr-none"
+            : "text-[color:var(--color-white)] bg-[color:var(--color-primary-400)] rounded-[8px] rounded-tl-none"
+        } p-2 text-xs w-fit`}
+      >
+        {text}
+      </div>
+    </div>
+  );
+};
+
+export default TextMessage;
