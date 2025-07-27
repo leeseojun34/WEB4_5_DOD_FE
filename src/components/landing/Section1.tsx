8f9c04d9b08865aefd3c6661493d8049a968b194
Jungyu Park <parkjungyu09@gmail.com>
2025-07-27 22:06:15 +0900

    Style: Section1 퍼블리싱

---
+++ src/components/landing/Section1.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import MainLogo from "./MainLogo";
+import SectionLayout from "./SectionLayout";
+
+const Section1 = () => {
+  return (
+    <SectionLayout>
+      <MainLogo />
+    </SectionLayout>
+  );
+};
+export default Section1;
