481119f170477f0d2ff14e347d19f258aaf7b3e2
Jungyu Park <parkjungyu09@gmail.com>
2025-07-28 10:23:16 +0900

    Style: 랜딩페이지 섹션 분리

--- src/components/landing/SectionLayout.tsx
+++ src/components/landing/SectionLayout.tsx
@@ -1,31 +1,47 @@
 "use client";
 
 import { useRouter } from "next/navigation";
-import { Bubble } from "../ui/Bubble";
 import { Button } from "../ui/Button";
 import { ReactNode } from "react";
+
+const sectionStyles: Record<string, string> = {
+  section1:
+    "flex flex-col px-10 justify-center items-center min-h-screen relative",
+  section2:
+    "flex flex-col px-10 justify-center items-center min-h-screen relative",
+  section3:
+    "flex flex-col px-10 justify-center items-center min-h-screen relative",
+  section4:
+    "flex flex-col px-0 justify-center items-center min-h-screen relative",
+  section5:
+    "flex flex-col px-10 justify-center items-center min-h-screen relative",
+  section6:
+    "flex flex-col px-10 justify-center items-center min-h-screen relative",
+};
 
 const SectionLayout = ({
   children,
   bgColor,
+  sectionNum,
 }: {
   children: ReactNode;
   bgColor?: string;
+  sectionNum: string;
 }) => {
   const router = useRouter();
 
   return (
     <section
+      id={sectionNum}
       className={`${
         bgColor === "blue"
           ? "bg-[color:var(--color-primary-400)]"
           : "bg-[color:var(--color-white)]"
       }`}
     >
-      <main className="flex flex-col px-5 justify-center items-center min-h-screen relative">
+      <main className={sectionStyles[sectionNum]}>
         {children}
         <div className="fixed bottom-9 w-full px-5 flex items-center gap-3 flex-col z-50">
-          {/* <Bubble>복잡한 일정 조율, 한 번에 끝내세요 🔥</Bubble> */}
           <Button state="default" onClick={() => router.push("/auth/login")}>
             이때어때 시작하기
           </Button>
