95e4afb4eae40bbfd297545846362edb17d7a5fa
Jungyu Park <parkjungyu09@gmail.com>
2025-07-28 11:44:39 +0900

    Feat: section4 gsap 애니메이션 추가

--- src/components/landing/Section4.tsx
+++ src/components/landing/Section4.tsx
@@ -4,33 +4,113 @@ import Image from "next/image";
 import SectionLayout from "./SectionLayout";
 import groupManageImg from "@/assets/images/landing_group_management.png";
 import groupAnalyticsImg from "@/assets/images/landing_group_analytics.png";
+import { useEffect, useRef } from "react";
+import gsap from "gsap";
 
 const Section4 = () => {
+  const titleRef = useRef<HTMLDivElement>(null);
+  const groupManagementImgRef = useRef<HTMLImageElement>(null);
+  const groupAnalyticsImgRef = useRef<HTMLImageElement>(null);
+  const subtitle1Ref = useRef<HTMLDivElement>(null);
+  const subtitle2Ref = useRef<HTMLDivElement>(null);
+  const description1Ref = useRef<HTMLDivElement>(null);
+  const description2Ref = useRef<HTMLDivElement>(null);
+  useEffect(() => {
+    if (!titleRef.current) return;
+
+    const timeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: titleRef.current,
+        start: "top 90%",
+        end: "top 10%",
+        scrub: 2,
+      },
+    });
+
+    timeline.fromTo(
+      titleRef.current,
+      { y: 50, opacity: 0 },
+      { y: 0, opacity: 1, ease: "power4.out", duration: 2 }
+    );
+
+    timeline.fromTo(
+      [subtitle1Ref.current, groupManagementImgRef.current],
+      { y: 50, opacity: 0 },
+      { y: 0, opacity: 1, ease: "power4.out", duration: 2 },
+      "+=0.2"
+    );
+
+    timeline.fromTo(
+      description1Ref.current,
+      { y: 50, opacity: 0 },
+      { y: 0, opacity: 1, ease: "power4.out", duration: 2 },
+      "+=0.2"
+    );
+
+    timeline.fromTo(
+      [subtitle2Ref.current, groupAnalyticsImgRef.current],
+      { y: 50, opacity: 0 },
+      { y: 0, opacity: 1, ease: "power4.out", duration: 2 },
+      "+=0.2"
+    );
+
+    timeline.fromTo(
+      description2Ref.current,
+      { y: 50, opacity: 0 },
+      { y: 0, opacity: 1, ease: "power4.out", duration: 2 },
+      "+=0.2"
+    );
+  }, []);
   return (
     <SectionLayout sectionNum="section4">
-      <div className="text-[28px] font-bold text-[color:var(--color-black)]">
+      <div
+        ref={titleRef}
+        className="text-[28px] font-bold text-[color:var(--color-black)]"
+      >
         <span className="text-[color:var(--color-primary-400)]">그룹</span>과
         함께, <br />더 체계적으로
       </div>
       <div className="flex items-center w-full">
         <div className="flex flex-col gap-5 flex-1 pl-10">
-          <div className="text-xl font-semibold text-[color:var(--color-black)]">
+          <div
+            ref={subtitle1Ref}
+            className="text-xl font-semibold text-[color:var(--color-black)]"
+          >
             간편한 그룹 관리
           </div>
-          <div className="text-sm text-[color:var(--color-gray)]">
+          <div
+            ref={description1Ref}
+            className="text-sm text-[color:var(--color-gray)]"
+          >
             정해진 멤버 내에서 <br />
             일정을 조율해요
           </div>
         </div>
-        <Image src={groupManageImg} alt="그룹 관리 이미지" unoptimized />
+        <Image
+          ref={groupManagementImgRef}
+          src={groupManageImg}
+          alt="그룹 관리 이미지"
+          unoptimized
+        />
       </div>
       <div className="flex items-center w-full">
-        <Image src={groupAnalyticsImg} alt="그룹 통계 이미지" unoptimized />
+        <Image
+          ref={groupAnalyticsImgRef}
+          src={groupAnalyticsImg}
+          alt="그룹 통계 이미지"
+          unoptimized
+        />
         <div className="flex flex-col gap-5 flex-1 pr-10">
-          <div className="text-xl font-semibold text-[color:var(--color-black)] text-end">
+          <div
+            ref={subtitle2Ref}
+            className="text-xl font-semibold text-[color:var(--color-black)] text-end"
+          >
             모임 통계도 한눈에
           </div>
-          <div className="text-sm text-[color:var(--color-gray)] text-end">
+          <div
+            ref={description2Ref}
+            className="text-sm text-[color:var(--color-gray)] text-end"
+          >
             요일별 모임, 멤버별
             <br /> 참여율까지 분석해드려요
           </div>
