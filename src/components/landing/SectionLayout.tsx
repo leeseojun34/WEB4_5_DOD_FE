bf542bea43563df4b1e74c707eb7133254116435
Jungyu Park <parkjungyu09@gmail.com>
2025-07-29 00:13:32 +0900

    Feat: 섹션 진입 감지를 통해 Buble 컴포넌트 조건부 렌더링

--- src/components/landing/SectionLayout.tsx
+++ src/components/landing/SectionLayout.tsx
@@ -2,7 +2,8 @@
 
 import { useRouter } from "next/navigation";
 import { Button } from "../ui/Button";
-import { ReactNode } from "react";
+import { Bubble } from "../ui/Bubble";
+import { useEffect, useRef, useState, ReactNode } from "react";
 
 const sectionStyles: Record<string, string> = {
   section1:
@@ -19,36 +20,56 @@ const sectionStyles: Record<string, string> = {
     "flex flex-col px-10 justify-center items-center min-h-screen relative min-w-[375px] w-full max-w-[740px]",
 };
 
+interface SectionLayoutProps {
+  children: ReactNode;
+  bgColor?: string;
+  sectionNum: string;
+}
+
 const SectionLayout = ({
   children,
   bgColor,
   sectionNum,
-}: {
-  children: ReactNode;
-  bgColor?: string;
-  sectionNum: string;
-}) => {
+}: SectionLayoutProps) => {
   const router = useRouter();
+  const sectionRef = useRef<HTMLElement>(null);
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    if (!sectionRef.current) return;
+
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        setIsVisible(entry.isIntersecting);
+      },
+      { threshold: 1 }
+    );
+
+    observer.observe(sectionRef.current);
+    return () => observer.disconnect();
+  }, []);
+
+  const backgroundColorClass =
+    bgColor === "blue"
+      ? "bg-[color:var(--color-primary-400)]"
+      : bgColor === "skyblue"
+      ? "bg-[color:var(--color-primary-100)]"
+      : "bg-[color:var(--color-white)]";
 
   return (
     <section
+      ref={sectionRef}
       id={sectionNum}
-      className={`${
-        bgColor === "blue"
-          ? "bg-[color:var(--color-primary-400)]"
-          : bgColor === "skyblue"
-          ? "bg-[color:var(--color-primary-100)]"
-          : "bg-[color:var(--color-white)]"
-      } flex justify-center`}
+      className={`${backgroundColorClass} flex justify-center`}
     >
       <main className={sectionStyles[sectionNum]}>
         {children}
+
         <div className="fixed bottom-9 w-full px-5 flex items-center gap-3 flex-col z-50">
-          <Button
-            state="default"
-            onClick={() => router.push("/auth/login")}
-            className="shadow-sm"
-          >
+          {["section1", "section6"].includes(sectionNum) && isVisible && (
+            <Bubble>복잡한 일정 조율, 한 번에 끝내세요 🔥</Bubble>
+          )}
+          <Button state="default" onClick={() => router.push("/auth/login")}>
             이때어때 시작하기
           </Button>
         </div>
