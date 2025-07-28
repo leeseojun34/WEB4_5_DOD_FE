c712b053f0f75fc8551578a86c028154e6be0d74
Jungyu Park <parkjungyu09@gmail.com>
2025-07-28 23:54:02 +0900

    Style: 버튼 쉐도우 추가

--- src/components/landing/SectionLayout.tsx
+++ src/components/landing/SectionLayout.tsx
@@ -6,17 +6,17 @@ import { ReactNode } from "react";
 
 const sectionStyles: Record<string, string> = {
   section1:
-    "flex flex-col px-10 justify-center items-center min-h-screen relative min-w-[375px] w-full max-w-185",
+    "flex flex-col px-10 justify-center items-center min-h-screen relative min-w-[375px] w-full max-w-[740px]",
   section2:
-    "flex flex-col px-10 justify-center items-center min-h-screen relative min-w-[375px] w-full max-w-185",
+    "flex flex-col px-10 justify-center items-center min-h-screen relative min-w-[375px] w-full max-w-[740px]",
   section3:
-    "flex flex-col px-10 justify-center items-center min-h-screen relative min-w-[375px] w-full max-w-185",
+    "flex flex-col px-10 justify-center items-center min-h-screen relative min-w-[375px] w-full max-w-[740px]",
   section4:
-    "flex flex-col px-0 justify-center items-center min-h-screen relative min-w-[375px] w-full max-w-185",
+    "flex flex-col px-0 justify-center items-center min-h-screen relative min-w-[375px] w-full max-w-[740px]",
   section5:
-    "flex flex-col px-10 justify-center items-center min-h-screen relative min-w-[375px] w-full max-w-185",
+    "flex flex-col px-10 justify-center items-center min-h-screen relative min-w-[375px] w-full max-w-[740px]",
   section6:
-    "flex flex-col px-10 pt-24 items-center min-h-screen relative min-w-[375px] w-full max-w-185",
+    "flex flex-col px-10 justify-center items-center min-h-screen relative min-w-[375px] w-full max-w-[740px]",
 };
 
 const SectionLayout = ({
@@ -44,7 +44,11 @@ const SectionLayout = ({
       <main className={sectionStyles[sectionNum]}>
         {children}
         <div className="fixed bottom-9 w-full px-5 flex items-center gap-3 flex-col z-50">
-          <Button state="default" onClick={() => router.push("/auth/login")}>
+          <Button
+            state="default"
+            onClick={() => router.push("/auth/login")}
+            className="shadow-sm"
+          >
             이때어때 시작하기
           </Button>
         </div>
