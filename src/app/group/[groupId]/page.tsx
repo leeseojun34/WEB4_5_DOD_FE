081aec7888a29e7c9cd5d1cebb0aeb56e4e7437a
Jungyu Park <parkjungyu09@gmail.com>
2025-07-29 11:39:51 +0900

    Fix: 그룹 일정 생성 추가 방법 선택 페이지 user id authStore에서 값 가져와서 router에 전달 및 메타태그 설정

--- src/app/group/[groupId]/page.tsx
+++ src/app/group/[groupId]/page.tsx
@@ -1,6 +1,10 @@
-"use client";
-
 import GroupDetailPage from "@/components/feature/group/detail/GroupDetailPage";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "이때어때 - 그룹",
+  description: "이때어때 - 그룹",
+};
 
 const GroupPage = () => {
   return <GroupDetailPage />;
