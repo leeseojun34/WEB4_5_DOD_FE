31ee9a2a61aba095ca4c9481aca20c12a8d777c0
Jungyu Park <parkjungyu09@gmail.com>
2025-07-29 22:29:29 +0900

    Style: 영상 크기 조정

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,7 +1,7 @@
 const GlobalLoading = () => {
   return (
     <div className="bg-[color:var(--color-white)] w-full min-h-screen justify-center items-center flex flex-col">
-      <video autoPlay loop muted playsInline className="h-[200px]">
+      <video autoPlay loop muted playsInline className="w-50 h-50">
         <source src="/loading.mp4" />
       </video>
       <div className="font-[TTTogether] text-black">...LOADING</div>
